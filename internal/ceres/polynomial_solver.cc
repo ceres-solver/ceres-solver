c8fc1c8b898cb0bb024f8703d2b7b80e1bc733ca
Markus Moll <markus.moll@esat.kuleuven.be>
2012-07-25 11:34:59 +0200

    Add a polynomial solver
    Add a function to find the (complex) roots of a polynomial
    with real coefficients. The roots are extracted as the
    eigenvalues of the (balanced) companion matrix. Also adds a test.
    The polynomial solver will be needed in the Dogleg subspace
    strategy.
    
    Change-Id: Ia6626158819efb858522b7f4998649ca010d6688

---
+++ internal/ceres/polynomial_solver.cc
@@ -0,0 +1,179 @@
+// Ceres Solver - A fast non-linear least squares minimizer
+// Copyright 2012 Google Inc. All rights reserved.
+// http://code.google.com/p/ceres-solver/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of Google Inc. nor the names of its contributors may be
+//   used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include "ceres/polynomial_solver.h"
+
+#include <glog/logging.h>
+
+#include <cmath>
+#include <cstddef>
+
+#include "Eigen/Dense"
+#include "ceres/internal/port.h"
+
+namespace ceres {
+namespace internal {
+namespace {
+// Balancing function as described by Parlett and Reinsch,
+// "Balancing a Matrix for Calculation of Eigenvalues and
+// Eigenvectors".
+// In contrast to their description, this function includes
+// the diagonal in the computations, which slightly alters
+// the meaning of the parameter gamma.
+// For companion matrices, this is not a big issue, as the
+// diagonal is mostly zero (only the last diagonal entry
+// is non-zero).
+void BalanceCompanionMatrix(Matrix* companion_matrix_ptr) {
+  CHECK_NOTNULL(companion_matrix_ptr);
+  Matrix& companion_matrix = *companion_matrix_ptr;
+
+  const int degree = companion_matrix.rows();
+  const double gamma = 0.9;
+
+  // Greedily scale row/column pairs until there is no change.
+  bool scaling_has_changed;
+  do {
+    scaling_has_changed = false;
+
+    for (int i = 0; i < degree; ++i) {
+      // This computes the norm over the whole row/column, which
+      // is different from Parlett and Reinsch.
+      const double row_norm = companion_matrix.row(i).lpNorm<1>();
+      const double col_norm = companion_matrix.col(i).lpNorm<1>();
+
+      // Decompose row_norm/col_norm into mantissa * 2^exponent,
+      // where 0.5 <= mantissa < 1. Discard mantissa (return value
+      // of frexp), as only the exponent is needed.
+      int exponent = 0;
+      std::frexp(row_norm/col_norm, &exponent);
+      exponent /= 2;
+
+      if (exponent != 0) {
+        const double scaled_col_norm = std::ldexp(col_norm, exponent);
+        const double scaled_row_norm = std::ldexp(row_norm, -exponent);
+        if (scaled_col_norm + scaled_row_norm < gamma * (col_norm + row_norm)) {
+          // Accept the new scaling. (Multiplication by powers of 2 should not
+          // introduce rounding errors (ignoring non-normalized numbers and
+          // over- or underflow))
+          scaling_has_changed = true;
+          companion_matrix.row(i) *= std::ldexp(1.0, -exponent);
+          companion_matrix.col(i) *= std::ldexp(1.0, exponent);
+        }
+      }
+    }
+  } while (scaling_has_changed);
+
+  VLOG(3) << "Balanced companion matrix is\n" << companion_matrix;
+}
+
+void BuildCompanionMatrix(const Vector& polynomial,
+    Matrix* companion_matrix_ptr) {
+  CHECK_NOTNULL(companion_matrix_ptr);
+  Matrix& companion_matrix = *companion_matrix_ptr;
+
+  const int degree = polynomial.size() - 1;
+
+  companion_matrix.resize(degree, degree);
+  companion_matrix.setZero();
+  companion_matrix.diagonal(-1).setOnes();
+  companion_matrix.col(degree-1) = -polynomial.reverse().head(degree-1);
+}
+}
+
+bool FindPolynomialRoots(const Vector& polynomial_in,
+    Vector* real, Vector* imaginary) {
+  const double epsilon = std::numeric_limits<double>::epsilon();
+
+  Vector polynomial = polynomial_in;
+  int degree = polynomial.size() - 1;
+
+  if (degree < 0) {
+    // This is not a valid polynomial.
+    LOG(ERROR) << "Invalid polynomial of size 0 passed to FindPolynomialRoots";
+    return false;
+  }
+
+  // Divide by leading term
+  if (polynomial(0) == 0.0) {
+    LOG(ERROR) << "Leading polynomial coefficient is zero in "
+      "FindPolynomialRoots";
+    return false;
+  }
+
+  for (int i = 0; i < degree; ++i) {
+    polynomial(i+1) /= polynomial(0);
+  }
+  polynomial(0) = 1.0;
+
+  // Is the polynomial constant?
+  if (degree == 0) {
+    LOG(WARNING) << "Trying to extract roots from a constant "
+      "polynomial in FindPolynomialRoots";
+    return true;
+  }
+
+  // Separately handle linear polynomials.
+  if (degree == 1) {
+    if (real != NULL) {
+      real->resize(1);
+      (*real)(0) = -polynomial[1];
+    }
+    if (imaginary != NULL) {
+      imaginary->resize(1);
+      imaginary->setZero();
+    }
+  }
+
+  // The degree is now known to be at least 2.
+  // Build and balance the companion matrix to the polynomial.
+  Matrix companion_matrix(degree, degree);
+  BuildCompanionMatrix(polynomial, &companion_matrix);
+  BalanceCompanionMatrix(&companion_matrix);
+
+  // Find its (complex) eigenvalues.
+  Eigen::EigenSolver< Matrix > solver(companion_matrix, Eigen::EigenvaluesOnly);
+  if (solver.info() != Eigen::Success) {
+    LOG(ERROR) << "Failed to extract eigenvalues from companion matrix.";
+    return false;
+  }
+
+  // Output roots
+  if (real != NULL) {
+    *real = solver.eigenvalues().real();
+  } else {
+    LOG(WARNING) << "NULL pointer passed as real argument to FindPolynomialRoots. "
+     "Real parts of the roots will not be returned.";
+  }
+  if (imaginary != NULL) {
+    *imaginary = solver.eigenvalues().imag();
+  }
+  return true;
+}
+
+}  // namespace internal
+}  // namespace ceres
---
+++ internal/ceres/polynomial_solver_test.cc
@@ -0,0 +1,262 @@
+// Ceres Solver - A fast non-linear least squares minimizer
+// Copyright 2012 Google Inc. All rights reserved.
+// http://code.google.com/p/ceres-solver/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of Google Inc. nor the names of its contributors may be
+//   used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include "ceres/polynomial_solver.h"
+
+#include <limits>
+#include <cmath>
+#include <cstddef>
+#include <algorithm>
+#include "gtest/gtest.h"
+
+namespace ceres {
+namespace internal {
+namespace {
+// Return a value suitable for comparison of an error
+// abs(x - reference), assuming that up to units many
+// machine precision errors are allowed.
+double Epsilon(double reference, double units) {
+  return units * std::numeric_limits<double>::epsilon() * std::abs(reference);
+}
+
+// The same as above, where units is 64.
+double Epsilon(double reference) {
+  return Epsilon(reference, 64.0);
+}
+
+// Return the constant polynomial p(x) = 1.23.
+Vector ConstantPolynomial(double value) {
+  Vector poly(1);
+  poly(0) = value;
+  return poly;
+}
+
+// Return the polynomial p(x) = poly(x) * (x - root).
+Vector AddRealRoot(const Vector& poly, double root) {
+  Vector poly2(poly.size() + 1);
+  poly2.setZero();
+  poly2.head(poly.size()) += poly;
+  poly2.tail(poly.size()) -= root * poly;
+  return poly2;
+}
+
+// Return the polynomial 
+// p(x) = poly(x) * (x - real - imag*i) * (x - real + imag*i).
+Vector AddComplexRootPair(const Vector& poly, double real, double imag) {
+  Vector poly2(poly.size() + 2);
+  poly2.setZero();
+  // Multiply poly by x^2 - 2real + abs(real,imag)^2
+  poly2.head(poly.size()) += poly;
+  poly2.segment(1, poly.size()) -= 2 * real * poly;
+  poly2.tail(poly.size()) += (real*real + imag*imag) * poly;
+  return poly2;
+}
+
+// Sort the entries in a vector.
+// Needed because the roots are not returned in sorted order.
+Vector SortVector(const Vector& in) {
+  Vector out(in);
+  std::sort(out.data(), out.data() + out.size());
+  return out;
+}
+
+// Run a test with the polynomial defined by the N real roots in roots_real.
+// If use_real is false, NULL is passed as the real argument to FindPolynomialRoots.
+// If use_imaginary is false, NULL is passed as the imaginary argument to FindPolynomialRoots.
+template<int N>
+void RunPolynomialTestRealRoots(const double (&real_roots)[N], bool use_real, bool use_imaginary) {
+  Vector real;
+  Vector imaginary;
+  Vector poly = ConstantPolynomial(1.23);
+  for (int i = 0; i < N; ++i) {
+    poly = AddRealRoot(poly, real_roots[i]);
+  }
+  bool success = FindPolynomialRoots(poly, use_real ? &real : NULL, use_imaginary ? &imaginary : NULL);
+
+  EXPECT_EQ(success, true);
+  if (use_real) {
+    EXPECT_EQ(real.size(), N);
+    real = SortVector(real);
+    for (int i = 0; i < N; ++i) {
+      EXPECT_LT(std::abs(real(i) - real_roots[i]), Epsilon(real_roots[i] == 0 ? 1.0 : real_roots[i]));
+    }
+  }
+  if (use_imaginary) {
+    EXPECT_EQ(imaginary.size(), N);
+    for (int i = 0; i < N; ++i) {
+      EXPECT_LT(std::abs(imaginary(i)), Epsilon(1.0));
+    }
+  }
+}
+}
+
+// Check if an invalid polynomial (no coefficient) is rejected.
+TEST(PolynomialSolver, EmptyPoly) {
+  // Vector poly(0) is an ambiguous constructor call, so
+  // use the constructor with explicit column count.
+  Vector poly(0, 1);
+  Vector real;
+  Vector imag;
+  bool success = FindPolynomialRoots(poly, &real, &imag);
+
+  EXPECT_EQ(success, false);
+}
+
+// Check if no roots are returned for the constant polynomial
+// p(x) = 1.23
+TEST(PolynomialSolver, ConstantPoly) {
+  Vector poly = ConstantPolynomial(1.23);
+  Vector real;
+  Vector imag;
+  bool success = FindPolynomialRoots(poly, &real, &imag);
+
+  EXPECT_EQ(success, true);
+  EXPECT_EQ(real.size(), 0);
+  EXPECT_EQ(imag.size(), 0);
+}
+
+// Test p(x) = x - 42.42
+TEST(PolynomialSolver, LinearPolyPositiveRoot) {
+  const double roots[1] = { 42.42 };
+  RunPolynomialTestRealRoots(roots, true, true);
+}
+
+// Test p(x) = x + 42.42
+TEST(PolynomialSolver, LinearPolyNegativeRoot) {
+  const double roots[1] = { -42.42 };
+  RunPolynomialTestRealRoots(roots, true, true);
+}
+
+// Test p(x) = x - 42.42 only extracting the real parts.
+TEST(PolynomialSolver, LinearPolyOnlyRealPartPositive) {
+  const double roots[1] = { 42.42 };
+  RunPolynomialTestRealRoots(roots, true, false);
+}
+
+// Test p(x) = x + 42.42 only extracting the real parts.
+TEST(PolynomialSolver, LinearPolyOnlyRealPartNegative) {
+  const double roots[1] = { -42.42 };
+  RunPolynomialTestRealRoots(roots, true, false);
+}
+
+// Test p(x) = (x - 1.0)(x - 42.42)
+TEST(PolynomialSolver, QuadraticPolyReal) {
+  const double roots[2] = { 1.0, 42.42 };
+  RunPolynomialTestRealRoots(roots, true, true);
+}
+
+// Test p(x) = (x - 1.0)(x + 42.42)
+TEST(PolynomialSolver, QuadraticPolyRealMixedSign) {
+  const double roots[2] = { -42.42, 1.0 };
+  RunPolynomialTestRealRoots(roots, true, true);
+}
+
+// Test p(x) = (x - 1.0)(x - 42.42) only extracting real parts.
+TEST(PolynomialSolver, QuadraticPolyRealOnlyRealPart) {
+  const double roots[2] = { 1.0, 42.0 };
+  RunPolynomialTestRealRoots(roots, true, false);
+}
+
+// Test p(x) = (x - 1.0)(x + 42.42) only extracting real parts.
+TEST(PolynomialSolver, QuadraticPolyRealMixedSignOnlyRealPart) {
+  const double roots[2] = { -42.0, 1.0 };
+  RunPolynomialTestRealRoots(roots, true, false);
+}
+
+// Check if roots that are close to each other are correctly
+// computed. As this is more difficult, we relax the floating
+// point equality test a bit.
+// The polynomial is p(x) = (x - 42.42)(x - 42.43)
+TEST(PolynomialSolver, QuadraticPolyRealClose) {
+  const double roots[2] = { 42.42, 42.43 };
+  Vector real;
+  Vector imag;
+
+  Vector poly = ConstantPolynomial(1.23);
+  poly = AddRealRoot(poly, roots[0]);
+  poly = AddRealRoot(poly, roots[1]);
+  bool success = FindPolynomialRoots(poly, &real, &imag);
+
+  EXPECT_EQ(success, true);
+  EXPECT_EQ(real.size(), 2);
+  EXPECT_EQ(imag.size(), 2);
+  real = SortVector(real);
+  // With close roots, we need to relax the tests.
+  EXPECT_LT(std::abs(real(0) - roots[0]), Epsilon(roots[0], 1e5));
+  EXPECT_LT(std::abs(imag(0)), Epsilon(roots[0], 1e5));
+  EXPECT_LT(std::abs(real(1) - roots[1]), Epsilon(roots[1], 1e5));
+  EXPECT_LT(std::abs(imag(1)), Epsilon(roots[1], 1e5));
+}
+
+// Check if complex roots are computed correctly.
+// p(x) = (x - 42.42 - 4.2 i)(x - 42.42 + 4.2 i)
+//      = x^2 - 84.84 x + 1817.0964
+TEST(PolynomialSolver, QuadraticPolyComplex) {
+  Vector real;
+  Vector imag;
+
+  Vector poly = ConstantPolynomial(1.23);
+  poly = AddComplexRootPair(poly, 42.42, 4.2);
+  bool success = FindPolynomialRoots(poly, &real, &imag);
+
+  EXPECT_EQ(success, true);
+  EXPECT_EQ(real.size(), 2);
+  EXPECT_EQ(imag.size(), 2);
+  EXPECT_LT(std::abs(real(0) - 42.42), Epsilon(42.42));
+  EXPECT_LT(std::min(std::abs(imag(0) - 4.2), std::abs(imag(0) + 4.2)), Epsilon(4.2));
+  EXPECT_LT(std::abs(real(1) - 42.42), Epsilon(42.42));
+  EXPECT_LT(std::min(std::abs(imag(1) - 4.2), std::abs(imag(1) + 4.2)), Epsilon(4.2));
+  EXPECT_LT(std::abs(imag(0) + imag(1)), Epsilon(4.2));
+}
+
+// Test quartic polynomials where the roots are spread.
+TEST(PolynomialSolver, QuarticPolyOnlyRealPart) {
+  const double roots[4] = { 1.23e-4, 1.23e-1, 1.23e+2, 1.23e+5 };
+  RunPolynomialTestRealRoots(roots, true, false);
+}
+
+// Test quartic polynomial where the roots form two clusters.
+TEST(PolynomialSolver, QuarticPolyTightOnlyRealPart) {
+  const double roots[4] = { 1.23e-1, 2.46e-1, 1.23e+5, 2.46e+5 };
+  RunPolynomialTestRealRoots(roots, true, false);
+}
+
+// Test quartic polynomial where two roots are zero.
+TEST(PolynomialSolver, QuarticPolyTwoZerosOnlyRealPart) {
+  const double roots[4] = { -42.42, 0.0, 0.0, 42.42 };
+  RunPolynomialTestRealRoots(roots, true, false);
+}
+
+// Test the quartic monomial p(x) = x^4.
+TEST(PolynomialSolver, QuarticPolyAllZerosOnlyRealPart) {
+  const double roots[4] = { 0.0, 0.0, 0.0, 0.0 };
+  RunPolynomialTestRealRoots(roots, true, false);
+}
+
+}  // namespace internal
+}  // namespace ceres
