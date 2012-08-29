b3fff1213cad485bff09e8968579d47a97d77d71
Petter Strandmark <strandmark@google.com>
2012-08-29 09:39:56 -0700

    Caching the symbolic Cholesky factorization when using CXSparse
    Average factorization times for bundle adjustment test problem:
    SuiteSparse: 0.2794 s.
    CXSparse: 0.4039 s.
    CXSparse cached: 0.2399 s.
    
    CXSparse will still be slower, though, because it has to compute
    the transpose and J^T * J.
    
    Change-Id: If9cdaa3dd520bee84b56e5fd4953b56a93db6bde

--- internal/ceres/sparse_normal_cholesky_solver.h
+++ internal/ceres/sparse_normal_cholesky_solver.h
@@ -34,13 +34,10 @@
 #ifndef CERES_INTERNAL_SPARSE_NORMAL_CHOLESKY_SOLVER_H_
 #define CERES_INTERNAL_SPARSE_NORMAL_CHOLESKY_SOLVER_H_
 
-#ifndef CERES_NO_SUITESPARSE
-#include "cholmod.h"
-#include "ceres/suitesparse.h"
-#endif // CERES_NO_SUITESPARSE
-
+#include "ceres/cxsparse.h"
 #include "ceres/linear_solver.h"
 #include "ceres/internal/macros.h"
+#include "ceres/suitesparse.h"
 
 namespace ceres {
 namespace internal {
@@ -80,6 +77,11 @@ class SparseNormalCholeskySolver : public CompressedRowSparseMatrixSolver {
   cholmod_factor* factor_;
 #endif  // CERES_NO_SUITESPARSE
 
+#ifndef CERES_NO_CXSPARSE
+  CXSparse cxsparse_;
+  // Cached factorization
+  css* cxsparse_factor_;
+#endif  // CERES_NO_CXSPARSE
 
   const LinearSolver::Options options_;
   CERES_DISALLOW_COPY_AND_ASSIGN(SparseNormalCholeskySolver);
