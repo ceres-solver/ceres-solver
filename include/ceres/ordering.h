19b38e72e7b7dee12ace73efa081772d1dcf33bb
Sameer Agarwal <sameeragarwal@google.com>
2012-09-10 17:41:38 -0700

    Start of the new ordering API.
    Change-Id: I37b0f39011f590d54962ad3e1da1f42712008f82

---
+++ include/ceres/ordering.h
@@ -0,0 +1,127 @@
+// Ceres Solver - A fast non-linear least squares minimizer
+// Copyright 2012 Google Inc. All rights reserved.
+// http://code.google.com/p/ceres-solver/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of Google Inc. nor the names of its contributors may be
+//   used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Author: sameeragarwal@google.com (Sameer Agarwal)
+
+#include <map>
+#include <set>
+#include "ceres/internal/port.h"
+#include "glog/logging.h"
+
+namespace ceres {
+
+// The order in which variables are eliminated in a linear solver can
+// have a significant of impact on the efficiency and accuracy of the
+// method. e.g., when doing sparse Cholesky factorization, there are
+// matrices for which a good ordering will give a Cholesky factor with
+// O(n) storage, where as a bad ordering will result in an completely
+// dense factor.
+//
+// Ceres allows the user to provide varying amounts of hints to the
+// solver about the variable elimination ordering to use. This can
+// range from no hints, where the solver is free to decide the best
+// possible ordering based on the user's choices like the linear
+// solver being used, to an exact order in which the variables should
+// be eliminated, and a variety of possibilities in between. Instances
+// of the Ordering class are used to communicating this infornation to
+// Ceres.
+//
+// Formally an ordering is ordered partitioning of the parameter
+// blocks, i.e, each parameter block belongs to exactly one group, and
+// each group has a unique integer associated with it, that determines
+// its order in the set of groups.
+//
+// Given such an ordering, Ceres ensures that the parameter blocks in
+// the lowest numbered group are eliminated first, and then the
+// parmeter blocks in the next lowest numbered group and so on. Within
+// each group, Ceres is free to do as it chooses.
+//
+// Let us consider some examples:
+//
+// 1. So if the user wishes Ceres to take all decision, put all
+// parameter blocks in a single group.
+//
+// 2. If the user wishes to indicate the order exactly, then if there
+// are n parameter blocks, then she should create n groups with one
+// parameter block each, with the appropriate ids.
+//
+// 3. A particular case of interest is structure from motion
+// problems. Where the user has two options. The default is that all
+// the parameter blocks are in group 0 (or not pass an ordering at
+// all), and the solver will see that the user wants to use a Schur
+// type solver and will figure out the right elimination ordering.
+//
+// But if the user already knows what parameter blocks are points and
+// what are cameras, she can save preprocessing time it would be
+// useful to partition the parameter blocks into two groups - points
+// and cameras, where the group containing the point parameter blocks
+// has an id smaller than the group containing cameras.
+class Ordering {
+ public:
+  // Add a parameter block to a group with id group_id. If a group
+  // with this id does not exist, one is created. This method can be
+  // called any number of times for a parameter block.
+  void AddParameterBlock(double* parameter_block, int group_id) {
+    const map<double*, int>::const_iterator it =
+        parameter_block_to_group_id_.find(parameter_block);
+
+    if (it != parameter_block_to_group_id_.end()) {
+      const int current_group_id = it->second;
+      group_id_to_parameter_blocks_[group_id].erase(parameter_block);
+    }
+
+    parameter_block_to_group_id_[parameter_block] = group_id;
+    group_id_to_parameter_blocks_[group_id].insert(parameter_block);
+  }
+
+  // Return the group id for the parameter block. If the parameter
+  // block is not known to the Ordering, calling this method results
+  // in a crash.
+  int GetGroupId(double* parameter_block) const {
+    const map<double*, int>::const_iterator it =
+        parameter_block_to_group_id_.find(parameter_block);
+    CHECK(it !=  parameter_block_to_group_id_.end());
+    return it->second;
+  }
+
+  int NumParameterBlocks() const {
+    return parameter_block_to_group_id_.size();
+  }
+
+  int NumGroups() const { return group_id_to_parameter_blocks_.size(); }
+
+  const map<int, set<double*> >& group_id_to_parameter_blocks() const {
+    return group_id_to_parameter_blocks_;
+  }
+
+ private:
+  map<int, set<double*> > group_id_to_parameter_blocks_;
+  map<double*, int>  parameter_block_to_group_id_;
+};
+
+}  // namespace ceres
