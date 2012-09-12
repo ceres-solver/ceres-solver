6da1f87a6bf173b6cffb1ac7af4ee497c626fc0d
Keir Mierle <mierle@gmail.com>
2012-09-11 17:34:33 -0700

    Add Ceres version and ABI version defines to ceres.h
    This will make it possible to write code which detects Ceres
    versions and does different things with different versions.
    Ideally this wouldn't be necessary, but in practice it is
    sometimes useful.
    
    Change-Id: I8d9f56d664ef75706e87c9bd7954e709dd7c0278

--- include/ceres/ceres.h
+++ include/ceres/ceres.h
@@ -34,6 +34,9 @@
 #ifndef CERES_PUBLIC_CERES_H_
 #define CERES_PUBLIC_CERES_H_
 
+#define CERES_VERSION 1.3.0
+#define CERES_ABI_VERSION 1.3.0
+
 #include "ceres/autodiff_cost_function.h"
 #include "ceres/cost_function.h"
 #include "ceres/iteration_callback.h"
