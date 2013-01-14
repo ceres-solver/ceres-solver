57596a6ef2cede320746f90aa21119061344bed7
Sameer Agarwal <sameeragarwal@google.com>
2013-01-13 22:14:12 -0800

    Initial cost function to functor adapter implementation
    Bug fix in autodiff.h
    
    Change-Id: If8ba281a89fda976ef2ce10a5844a74c4ac7b84a

--- include/ceres/internal/autodiff.h
+++ include/ceres/internal/autodiff.h
@@ -390,7 +390,7 @@ struct AutoDiff {
         x.get() + jet8,
         x.get() + jet9,
     };
-    JetT* output = x.get() + jet9;
+    JetT* output = x.get() + N0 + N1 + N2 + N3 + N4 + N5 + N6 + N7 + N8 + N9;
 
 #define CERES_MAKE_1ST_ORDER_PERTURBATION(i) \
     if (N ## i) { \
