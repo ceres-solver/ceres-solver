72a7230133ada3c0848bd366591f2bedf1915c21
Sameer Agarwal <sameeragarwal@google.com>
2013-02-01 12:22:53 -0800

    Expand reporting of timing information.
    1. Add an ExecutionSummary object to record execution
       information about Ceres objects.
    2. Add an EventLogger object to log events in a function call.
    3. Add a ScopedExecutionTimer object to log times in ExecutionSummary.
    4. Instrument ProgramEvaluator and all the linear solvers
       to report their timing statistics.
    5. Connect the timing statistics to Summary::FullReport.
    6. Add high precision timer on unix systems using
       gettimeofday() call.
    7. Various minor clean ups all around.
    
    Change-Id: I5e09804b730b09535484124be7dbc1c58eccd1d4

--- internal/ceres/cgnr_solver.cc
+++ internal/ceres/cgnr_solver.cc
@@ -30,11 +30,12 @@
 
 #include "ceres/cgnr_solver.h"
 
-#include "glog/logging.h"
-#include "ceres/linear_solver.h"
+#include "ceres/block_jacobi_preconditioner.h"
 #include "ceres/cgnr_linear_operator.h"
 #include "ceres/conjugate_gradients_solver.h"
-#include "ceres/block_jacobi_preconditioner.h"
+#include "ceres/linear_solver.h"
+#include "ceres/wall_time.h"
+#include "glog/logging.h"
 
 namespace ceres {
 namespace internal {
@@ -49,6 +50,8 @@ LinearSolver::Summary CgnrSolver::Solve(
     const double* b,
     const LinearSolver::PerSolveOptions& per_solve_options,
     double* x) {
+  EventLogger event_logger;
+
   // Form z = Atb.
   scoped_array<double> z(new double[A->num_cols()]);
   std::fill(z.get(), z.get() + A->num_cols(), 0.0);
@@ -69,11 +72,16 @@ LinearSolver::Summary CgnrSolver::Solve(
   // Solve (AtA + DtD)x = z (= Atb).
   std::fill(x, x + A->num_cols(), 0.0);
   CgnrLinearOperator lhs(*A, per_solve_options.D);
+
+  event_logger.AddRelativeEvent("LinearSolver::Setup");
+
   ConjugateGradientsSolver conjugate_gradient_solver(options_);
-  return conjugate_gradient_solver.Solve(&lhs,
-                                         z.get(),
-                                         cg_per_solve_options,
-                                         x);
+  LinearSolver::Summary summary =
+      conjugate_gradient_solver.Solve(&lhs, z.get(), cg_per_solve_options, x);
+
+  event_logger.AddRelativeEvent("LinearSolver::Solve");
+  event_logger.AddAbsoluteEvent("LinearSolver::Total");
+  return summary;
 }
 
 }  // namespace internal
