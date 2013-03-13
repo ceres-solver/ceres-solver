ac56fc414e90ca3b2be225eb0f30e942478397b7
Sameer Agarwal <sameeragarwal@google.com>
2013-03-13 12:14:00 -0700

    Make Android.mk play better with the external consraints
    Change-Id: Ia0a1037d97c032a4ba1a9acbf4e04c192d12ee61

--- jni/Android.mk
+++ jni/Android.mk
@@ -73,11 +73,16 @@
 LOCAL_PATH := $(call my-dir)
 
 EIGEN_PATH := $(EIGEN_PATH)
-
-CERES_INCLUDE_PATHS := $(LOCAL_PATH)/../internal
+CERES_INCLUDE_PATHS := $(CERES_EXTRA_INCLUDES)
+CERES_INCLUDE_PATHS += $(LOCAL_PATH)/../internal
 CERES_INCLUDE_PATHS += $(LOCAL_PATH)/../internal/ceres
 CERES_INCLUDE_PATHS += $(LOCAL_PATH)/../include
+
+ifdef CERES_GLOG_DIR
+CERES_INCLUDE_PATHS += $(CERES_GLOG_DIR)
+else
 CERES_INCLUDE_PATHS += $(LOCAL_PATH)/../internal/ceres/miniglog
+endif
 CERES_SRC_PATH := ../internal/ceres
 
 include $(CLEAR_VARS)
@@ -135,7 +140,6 @@ LOCAL_SRC_FILES := $(CERES_SRC_PATH)/array_utils.cc \
                    $(CERES_SRC_PATH)/local_parameterization.cc \
                    $(CERES_SRC_PATH)/loss_function.cc \
                    $(CERES_SRC_PATH)/low_rank_inverse_hessian.cc \
-                   $(CERES_SRC_PATH)/miniglog/glog/logging.cc \
                    $(CERES_SRC_PATH)/minimizer.cc \
                    $(CERES_SRC_PATH)/normal_prior.cc \
                    $(CERES_SRC_PATH)/parameter_block_ordering.cc \
@@ -182,6 +186,10 @@ LOCAL_SRC_FILES := $(CERES_SRC_PATH)/array_utils.cc \
                    $(CERES_SRC_PATH)/generated/schur_eliminator_4_4_3.cc \
                    $(CERES_SRC_PATH)/generated/schur_eliminator_4_4_4.cc \
                    $(CERES_SRC_PATH)/generated/schur_eliminator_4_4_d.cc
+
+ifndef CERES_GLOG_DIR
+LOCAL_SRC_FILES += $(CERES_SRC_PATH)/miniglog/glog/logging.cc
+endif
 
 LOCAL_MODULE := ceres
 include $(BUILD_STATIC_LIBRARY)
