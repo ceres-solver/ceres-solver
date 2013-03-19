346b469723dda2536b73eb4d840581064332078b
Pablo Speciale <pablo.speciale@gmail.com>
2013-03-19 14:05:14 -0700

    Avoiding the Warning: "deprecated conversion from string constant to char*"
    Change-Id: Ifa47f9b0724f79c5c695828628c89818ddefd844

--- internal/ceres/sparse_normal_cholesky_solver.cc
+++ internal/ceres/sparse_normal_cholesky_solver.cc
@@ -215,7 +215,7 @@ LinearSolver::Summary SparseNormalCholeskySolver::SolveImplUsingSuiteSparse(
     }
 
     if (VLOG_IS_ON(2)) {
-      cholmod_print_common("Symbolic Analysis", ss_.mutable_cc());
+      cholmod_print_common(const_cast<char*>("Symbolic Analysis"), ss_.mutable_cc());
     }
   }
 
