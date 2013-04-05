487250eb27256a41d38c5037bdac9a09a3160edb
Sameer Agarwal <sameeragarwal@google.com>
2013-04-05 14:20:37 -0700

    Minor cleanups.
    1. Further BLAS and heap allocation cleanups in  schur_eliminator_impl.h
    2. Modularize blas.h using macros.
    3. Lint cleanups from William Rucklidge.
    4. Small changes to jet.h
    5. ResidualBlock now uses blas.h
    
    Performance improvements:
    
    For static and dynamic sized blocks, the peformance is not changed much.
    
    -use_quaternions -ordering user -linear_solver sparse_schur
    
                                            master         change
    problem: 16-22106
    gcc                                        3.4            3.3
    clang                                      2.8            2.7
    
    problem: 49-7776
    gcc                                        1.7            1.7
    clang                                      1.4            1.4
    
    problem: 245-198739
    gcc                                       80.1           79.6
    clang                                     80.6           76.2
    
    problem: 257-65132
    gcc                                       12.2           12.0
    clang                                     10.4           10.2
    
    problem: 356-226730
    gcc                                       99.0           96.8
    clang                                     88.9           88.3
    
    problem: 744-543562
    gcc                                      361.5          356.2
    clang                                    352.7          343.5
    
    problem: 1024-110968
    gcc                                       45.9           45.6
    clang                                     42.6           42.1
    
    However, performance when using local parameterizations is
    significantly improved due to residual_block.cc using blas.h
    
    -use_quaternions -use_local_parameterization -ordering user -linear_solver sparse_schur
    
                                            master         change
    problem: 16-22106
    gcc                                        3.6            3.3
    clang                                      3.5            2.8
    
    problem: 49-7776
    gcc                                        1.8            1.6
    clang                                      1.7            1.4
    
    problem: 245-198739
    gcc                                       79.7           76.1
    clang                                     79.7           73.0
    
    problem: 257-65132
    gcc                                       12.8           11.9
    clang                                     12.3            9.8
    
    problem: 356-226730
    gcc                                      101.9           93.5
    clang                                    105.0           86.8
    
    problem: 744-543562
    gcc                                      367.9          350.5
    clang                                    355.3          323.1
    
    problem: 1024-110968
    gcc                                       43.0           40.3
    clang                                     41.0           37.5
    
    Change-Id: I6dcf7476ddaa77cb116558d112a9cf1e832f5fc9

--- internal/ceres/residual_block.cc
+++ internal/ceres/residual_block.cc
@@ -35,6 +35,7 @@
 #include <cstddef>
 #include <vector>
 
+#include "ceres/blas.h"
 #include "ceres/corrector.h"
 #include "ceres/parameter_block.h"
 #include "ceres/residual_block_utils.h"
@@ -139,17 +140,15 @@ bool ResidualBlock::Evaluate(const bool apply_loss_function,
 
         // Apply local reparameterization to the jacobians.
         if (parameter_block->LocalParameterizationJacobian() != NULL) {
-          ConstMatrixRef local_to_global(
+          MatrixMatrixMultiply
+              <Eigen::Dynamic, Eigen::Dynamic, Eigen::Dynamic, Eigen::Dynamic, 0>(
+              global_jacobians[i],
+              num_residuals,
+              parameter_block->Size(),
               parameter_block->LocalParameterizationJacobian(),
               parameter_block->Size(),
-              parameter_block->LocalSize());
-          MatrixRef global_jacobian(global_jacobians[i],
-                                    num_residuals,
-                                    parameter_block->Size());
-          MatrixRef local_jacobian(jacobians[i],
-                                   num_residuals,
-                                   parameter_block->LocalSize());
-          local_jacobian.noalias() = global_jacobian * local_to_global;
+              parameter_block->LocalSize(),
+              jacobians[i], 0, 0,  num_residuals, parameter_block->LocalSize());
         }
       }
     }
