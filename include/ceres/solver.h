3bf779a7a13e0f05b33ff17900ae506cb73ed775
Sameer Agarwal <sameeragarwal@google.com>
2013-12-19 23:41:34 -0800

    General Nonlinear Problem solver.
    Change-Id: I8a151332f0eb11612dfd316411b90ce1d69c55ae

--- include/ceres/solver.h
+++ include/ceres/solver.h
@@ -43,6 +43,7 @@
 
 namespace ceres {
 
+class NonlinearProblem;
 class Problem;
 
 // Interface for non-linear least squares solvers.
@@ -938,11 +939,32 @@ class Solver {
   virtual void Solve(const Options& options,
                      Problem* problem,
                      Solver::Summary* summary);
+
+  virtual void Solve(const Solver::Options& options,
+                     const NonlinearProblem& problem,
+                     double* parameters,
+                     Solver::Summary* summary);
 };
 
 // Helper function which avoids going through the interface.
 void Solve(const Solver::Options& options,
            Problem* problem,
+           Solver::Summary* summary);
+
+// WARNING: THIS IS EXPERIMENTAL CODE. THE API WILL CHANGE.
+// WARNING: THIS IS EXPERIMENTAL CODE. THE API WILL CHANGE.
+// WARNING: THIS IS EXPERIMENTAL CODE. THE API WILL CHANGE.
+// WARNING: THIS IS EXPERIMENTAL CODE. THE API WILL CHANGE.
+// WARNING: THIS IS EXPERIMENTAL CODE. THE API WILL CHANGE.
+// WARNING: THIS IS EXPERIMENTAL CODE. THE API WILL CHANGE.
+// WARNING: THIS IS EXPERIMENTAL CODE. THE API WILL CHANGE.
+
+// Solve a non-linear problem using the line search based
+// minimizer. All options related to the trust region minimizer are
+// ignored.
+void Solve(const Solver::Options& options,
+           const NonlinearProblem& problem,
+           double* parameters,
            Solver::Summary* summary);
 
 }  // namespace ceres
