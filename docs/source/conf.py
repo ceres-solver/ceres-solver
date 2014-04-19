cc9d3bba1008066e51502cabd956985c6bdedfe8
Sameer Agarwal <sameeragarwal@google.com>
2014-04-18 22:58:09 -0700

    Remove a comment from conf.py
    Change-Id: I675f7e8fc5dd2143eab74901bc7241e02e37285f

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -91,7 +91,6 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'flask'
 html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
