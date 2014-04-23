455dad67741dfcef47a7a2bf78edf0a4e5425978
Richard Stebbing <richie.stebbing@gmail.com>
2014-04-23 22:17:26 +0100

    Add dynamic_sparsity option.
    The standard sparse normal Cholesky solver assumes a fixed sparsity pattern
    which is useful for a large number of problems presented to Ceres. However,
    some problems are symbolically dense but numerically sparse i.e. each residual
    is a function of a large number of parameters but at any given state the
    residual only depends on a sparse subset of them. For these class of problems
    it is faster to re-analyse the sparsity pattern of the jacobian at each
    iteration of the non-linear optimisation instead of including all of the zero
    entries in the step computation.
    
    The proposed solution adds the dynamic_sparsity option which can be used
    with SPARSE_NORMAL_CHOLESKY. A DynamicCompressedRowSparseMatrix type
    (which extends CompressedRowSparseMatrix) has been introduced which
    allows dynamic addition and removal of elements. A Finalize method is
    provided which then consolidates the matrix so that it can be used in
    place of a regular CompressedRowSparseMatrix. An associated jacobian
    writer has also been providedd.
    
    Changes that were required to make this extension were adding the
    SetMaxNumNonZeros to CompressedRowSparseMatrix and adding a
    JacobianFinalizer template parameter to the ProgramEvaluator.
    
    Change-Id: Ia5a8a9523fdae8d5b027bc35e70b4611ec2a8d01

--- internal/ceres/sparse_normal_cholesky_solver.cc
+++ internal/ceres/sparse_normal_cholesky_solver.cc
@@ -56,13 +56,13 @@ SparseNormalCholeskySolver::SparseNormalCholeskySolver(
       options_(options) {
 }
 
-SparseNormalCholeskySolver::~SparseNormalCholeskySolver() {
+void SparseNormalCholeskySolver::FreeFactorization() {
 #ifndef CERES_NO_SUITESPARSE
   if (factor_ != NULL) {
     ss_.Free(factor_);
     factor_ = NULL;
   }
-#endif
+#endif  // CERES_NO_SUITESPARSE
 
 #ifndef CERES_NO_CXSPARSE
   if (cxsparse_factor_ != NULL) {
@@ -70,6 +70,10 @@ SparseNormalCholeskySolver::~SparseNormalCholeskySolver() {
     cxsparse_factor_ = NULL;
   }
 #endif  // CERES_NO_CXSPARSE
+}
+
+SparseNormalCholeskySolver::~SparseNormalCholeskySolver() {
+  FreeFactorization();
 }
 
 LinearSolver::Summary SparseNormalCholeskySolver::SolveImpl(
@@ -150,13 +154,20 @@ LinearSolver::Summary SparseNormalCholeskySolver::SolveImplUsingCXSparse(
   event_logger.AddEvent("Setup");
 
   // Compute symbolic factorization if not available.
+  if (options_.dynamic_sparsity) {
+    FreeFactorization();
+  }
   if (cxsparse_factor_ == NULL) {
     if (options_.use_postordering) {
       cxsparse_factor_ = cxsparse_.BlockAnalyzeCholesky(AtA,
                                                         A->col_blocks(),
                                                         A->col_blocks());
     } else {
-      cxsparse_factor_ = cxsparse_.AnalyzeCholeskyWithNaturalOrdering(AtA);
+      if (options_.dynamic_sparsity) {
+        cxsparse_factor_ = cxsparse_.AnalyzeCholesky(AtA);
+      } else {
+        cxsparse_factor_ = cxsparse_.AnalyzeCholeskyWithNaturalOrdering(AtA);
+      }
     }
   }
   event_logger.AddEvent("Analysis");
@@ -169,6 +180,7 @@ LinearSolver::Summary SparseNormalCholeskySolver::SolveImplUsingCXSparse(
     summary.termination_type = LINEAR_SOLVER_FAILURE;
   }
   event_logger.AddEvent("Solve");
+
   return summary;
 }
 #else
@@ -198,6 +210,9 @@ LinearSolver::Summary SparseNormalCholeskySolver::SolveImplUsingSuiteSparse(
   cholmod_sparse lhs = ss_.CreateSparseMatrixTransposeView(A);
   event_logger.AddEvent("Setup");
 
+  if (options_.dynamic_sparsity) {
+    FreeFactorization();
+  }
   if (factor_ == NULL) {
     if (options_.use_postordering) {
       factor_ = ss_.BlockAnalyzeCholesky(&lhs,
@@ -205,7 +220,11 @@ LinearSolver::Summary SparseNormalCholeskySolver::SolveImplUsingSuiteSparse(
                                          A->row_blocks(),
                                          &summary.message);
     } else {
-      factor_ = ss_.AnalyzeCholeskyWithNaturalOrdering(&lhs, &summary.message);
+      if (options_.dynamic_sparsity) {
+        factor_ = ss_.AnalyzeCholesky(&lhs, &summary.message);
+      } else {
+        factor_ = ss_.AnalyzeCholeskyWithNaturalOrdering(&lhs, &summary.message);
+      }
     }
   }
   event_logger.AddEvent("Analysis");
