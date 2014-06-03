52bad6223bf41598804387ad073e55c672c8d294
Alex Stewart <alexs.mac@gmail.com>
2014-06-03 21:40:20 +0100

    Adding assert_ndk_version for Android from Jørgen Tjernø.
    - Ceres now requires Android NDK version >= r9d to compile, Jørgen's
      script allows a clean way to verify this condition is met before
      trying to compile for Android.
    - Note that this is a modified version of the original script, which
      adds support for versions >= 10, and fixes extraction of the version
      number now that the architecture is also included.
    
    Change-Id: Icd7ffd25407bcf29af007e30b5da5d18eb799adf

--- jni/Android.mk
+++ jni/Android.mk
@@ -74,6 +74,11 @@
 
 LOCAL_PATH := $(call my-dir)
 
+# Ceres requires at least NDK version r9d to compile.
+ifneq ($(shell $(LOCAL_PATH)/assert_ndk_version.sh "r9d"), true)
+  $(error Ceres requires NDK version r9d or greater)
+endif
+
 EIGEN_PATH := $(EIGEN_PATH)
 CERES_INCLUDE_PATHS := $(CERES_EXTRA_INCLUDES)
 CERES_INCLUDE_PATHS += $(LOCAL_PATH)/../internal
