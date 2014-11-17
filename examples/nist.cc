352e4dd182abbbff01ceb8bfc84482b34100d3c3
Sameer Agarwal <sameeragarwal@google.com>
2014-11-17 06:50:41 -0800

    Stricter options checking breaks nist.cc
    Fix the breakage!
    
    Change-Id: Ic263852cd7c8a9615d8d3dab43c6cf7d81fc8d40

--- examples/nist.cc
+++ examples/nist.cc
@@ -92,7 +92,7 @@ DEFINE_string(linear_solver, "dense_qr", "Options are: "
               "cgnr");
 DEFINE_string(preconditioner, "jacobi", "Options are: "
               "identity, jacobi");
-DEFINE_string(line_search, "armijo",
+DEFINE_string(line_search, "wolfe",
               "Line search algorithm to use, choices are: armijo and wolfe.");
 DEFINE_string(line_search_direction, "lbfgs",
               "Line search direction algorithm to use, choices: lbfgs, bfgs");
