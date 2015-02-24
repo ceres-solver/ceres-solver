a8ea64b52e8652c83094f985245549fe2f4de5d5
Richard Stebbing <richie.stebbing@gmail.com>
2015-02-24 09:55:01 -0800

    Add ComposedCostFunction
    At present, function composition in Ceres is done using functors. This
    is perfect for cost functions with a fixed number of parameters (known
    at compile time), but it is not suitable for some cost functions where
    the number of parameters isn't known until run time and when there are
    a lot of them.
    
    An example of this is fitting a subdivision surface to data points
    under an external transformation. In this case, the subdivision surface
    `CostFunction` can be quite complex and take a large number of arguments
    (control points and preimages), but the functor for rotating an
    evaluated position, scaling it, translating it, and determining the
    residual error is simple. `ComposedCostFunction` enables easy and
    efficient composition of these two functions.
    
    Change-Id: Ia731121c70606eff106e764374a36255773c59f1

---
+++ include/ceres/composed_cost_function.h
@@ -0,0 +1,87 @@
+// Ceres Solver - A fast non-linear least squares minimizer
+// Copyright 2015 Google Inc. All rights reserved.
+// http://code.google.com/p/ceres-solver/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of Google Inc. nor the names of its contributors may be
+//   used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Author: richie.stebbing@gmail.com (Richard Stebbing)
+
+#ifndef CERES_PUBLIC_COST_FUNCTION_COMPOSITION_H_
+#define CERES_PUBLIC_COST_FUNCTION_COMPOSITION_H_
+
+#include <vector>
+
+#include "ceres/cost_function.h"
+#include "ceres/internal/scoped_ptr.h"
+#include "ceres/types.h"
+
+namespace ceres {
+
+class CERES_EXPORT ComposedCostFunction : public CostFunction {
+ public:
+  ComposedCostFunction(CostFunction* f,
+                       Ownership ownership=TAKE_OWNERSHIP);
+
+  virtual ~ComposedCostFunction();
+
+  void AddInputCostFunction(CostFunction* g,
+                            const std::vector<double*>& parameter_blocks,
+                            Ownership ownership=TAKE_OWNERSHIP);
+
+  void AddInputParameterBlock(double* values, int size);
+
+  void Finalize();
+
+  virtual bool Evaluate(const double* const* parameters,
+                        double* residuals,
+                        double** jacobians) const;
+
+  const std::vector<double*>& parameter_blocks() const {
+    return parameter_blocks_;
+  }
+
+ private:
+  struct ComposedCostFunctionInput;
+
+  int AddInternalParameterBlock(double* values, int size);
+  bool InternalEvaluate(int input_index,
+                        const double* const* parameters,
+                        double* residuals,
+                        double** jacobians) const;
+
+ private:
+  internal::scoped_ptr<CostFunction> f_;
+  Ownership owns_f_;
+  bool is_finalised_;
+
+  std::vector<ComposedCostFunctionInput*> inputs_;
+  std::vector<double*> parameter_blocks_;
+  std::vector<std::vector<std::pair<int, int> > >
+    parameter_to_jacobian_blocks_;
+};
+
+}  // namespace ceres
+
+#endif  // CERES_PUBLIC_COST_FUNCTION_COMPOSITION_H_
