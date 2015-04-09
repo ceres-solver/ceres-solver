152d281682c9e34031d72dd6941daeedf165fd9c
pmoulon <pmoulon@gmail.com>
2015-04-09 18:02:59 +0200

    Add PLY file logger before and after BA in order to ease visual comparison.
    Change-Id: Ib14e8f4b2de686ab6494de270458392f81a0b946

--- examples/bundle_adjuster.cc
+++ examples/bundle_adjuster.cc
@@ -120,6 +120,9 @@ DEFINE_int32(random_seed, 38401, "Random seed used to set the state "
              "the pertubations.");
 DEFINE_bool(line_search, false, "Use a line search instead of trust region "
             "algorithm.");
+DEFINE_string(initial_ply, "", "Export the BAL file data as a PLY file.");
+DEFINE_string(final_ply, "", "Export the refined BAL file data as a PLY "
+            "file.");
 
 namespace ceres {
 namespace examples {
@@ -311,6 +314,10 @@ void BuildProblem(BALProblem* bal_problem, Problem* problem) {
 
 void SolveProblem(const char* filename) {
   BALProblem bal_problem(filename, FLAGS_use_quaternions);
+
+  if (!FLAGS_initial_ply.empty())
+    bal_problem.WriteToPLYFile(FLAGS_initial_ply.c_str());
+
   Problem problem;
 
   srand(FLAGS_random_seed);
@@ -327,6 +334,9 @@ void SolveProblem(const char* filename) {
   Solver::Summary summary;
   Solve(options, &problem, &summary);
   std::cout << summary.FullReport() << "\n";
+
+  if (!FLAGS_final_ply.empty())
+    bal_problem.WriteToPLYFile(FLAGS_final_ply.c_str());
 }
 
 }  // namespace examples
