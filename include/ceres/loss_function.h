26ed5367b4a9858f05ba7f3f34904cdcc7fe8eb3
Simon Rutishauser <simon.rutishauser@pix4d.com>
2015-05-19 12:44:23 +0200

    Allow the LossFunction contained in a LossFunctionWrapper to be NULL. This is consistent with how NULL LossFunctions are treated everywhere else.
    Change-Id: Ic91e39ccb13137fcad7f85e78613a29ecde30d67

--- include/ceres/loss_function.h
+++ include/ceres/loss_function.h
@@ -358,6 +358,9 @@ class CERES_EXPORT ScaledLoss : public LossFunction {
 // whose scale can be mutated after an optimization problem has been
 // constructed.
 //
+// Since we treat the a NULL Loss function as the Identity loss
+// function, rho = NULL is a valid input.
+//
 // Example usage
 //
 //  Problem problem;
@@ -394,8 +397,9 @@ class CERES_EXPORT LossFunctionWrapper : public LossFunction {
   }
 
   virtual void Evaluate(double sq_norm, double out[3]) const {
-    CHECK_NOTNULL(rho_.get());
-    rho_->Evaluate(sq_norm, out);
+    if (rho_.get() != NULL) {
+      rho_->Evaluate(sq_norm, out);
+    }
   }
 
   void Reset(LossFunction* rho, Ownership ownership) {
