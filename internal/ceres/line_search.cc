8fb71dae1582018469770586d157993c00b56a93
Sameer Agarwal <sameeragarwal@google.com>
2017-06-28 21:16:39 -0700

    Get rid of redundant function evaluations in LineSearchMinimizer
    1. Replace LineSearch::Summary::optimal_step_size with
       LineSearch:Summary::optimal_point which is a FunctionSample.
    2. Add the actual vector position and vector gradient of the
       point in the FunctionSample
    3. Use the above two to get rid of an extraneous function evalation
       in LineSearchMinimizer.
    
    Runtime performance is almost 2x improved as a result.
    
    Thanks to @svenpilz for reporting this.
    
    https://github.com/ceres-solver/ceres-solver/issues/296
    
    Change-Id: Iebf2db7acecb2c95c9b1683b73cdc5faab78b02e

--- internal/ceres/line_search.cc
+++ internal/ceres/line_search.cc
@@ -125,6 +125,8 @@ void LineSearchFunction::Init(const Vector& position,
   direction_ = direction;
 }
 
+// TODO(sameeragarwal): Likely can remove the internal storage in
+// LineSearchFunction.
 void LineSearchFunction::Evaluate(const double x,
                                   const bool evaluate_gradient,
                                   FunctionSample* output) {
@@ -139,6 +141,8 @@ void LineSearchFunction::Evaluate(const double x,
     return;
   }
 
+  output->vector_x = evaluation_point_;
+
   const bool eval_status =
       evaluator_->Evaluate(evaluation_point_.data(),
                            &(output->value),
@@ -151,17 +155,22 @@ void LineSearchFunction::Evaluate(const double x,
   }
 
   output->value_is_valid = true;
-  if (evaluate_gradient) {
-    output->gradient = direction_.dot(gradient_);
+  if (!evaluate_gradient) {
+    return;
   }
+
+  output->gradient = direction_.dot(gradient_);
   output->gradient_is_valid = IsFinite(output->gradient);
-  return;
+  if (output->gradient_is_valid) {
+    output->vector_gradient = gradient_;
+  }
 }
 
 double LineSearchFunction::DirectionInfinityNorm() const {
   return direction_.lpNorm<Eigen::Infinity>();
 }
 
+// TODO(sameeragarwal): Track down this gradient residual time thing.
 void LineSearchFunction::ResetTimeStatistics() {
   const map<string, double> evaluator_time_statistics =
       evaluator_->TimeStatistics();
@@ -200,7 +209,6 @@ void LineSearch::Search(double step_size_estimate,
   summary->cost_evaluation_time_in_seconds = 0.0;
   summary->gradient_evaluation_time_in_seconds = 0.0;
   summary->polynomial_minimization_time_in_seconds = 0.0;
-
   options().function->ResetTimeStatistics();
   this->DoSearch(step_size_estimate, initial_cost, initial_gradient, summary);
   options().function->
@@ -365,7 +373,7 @@ void ArmijoLineSearch::DoSearch(const double step_size_estimate,
     function->Evaluate(step_size, kEvaluateGradient, &current);
   }
 
-  summary->optimal_step_size = current.x;
+  summary->optimal_point = current;
   summary->success = true;
 }
 
@@ -431,7 +439,7 @@ void WolfeLineSearch::DoSearch(const double step_size_estimate,
     // produce a valid point when ArmijoLineSearch would succeed, we return the
     // point with the lowest cost found thus far which satsifies the Armijo
     // condition (but not the Wolfe conditions).
-    summary->optimal_step_size = bracket_low.x;
+    summary->optimal_point = bracket_low;
     summary->success = true;
     return;
   }
@@ -477,11 +485,13 @@ void WolfeLineSearch::DoSearch(const double step_size_estimate,
   // satisfies the strong Wolfe curvature condition, that we return the point
   // amongst those found thus far, which minimizes f() and satisfies the Armijo
   // condition.
-  solution =
-      solution.value_is_valid && solution.value <= bracket_low.value
-      ? solution : bracket_low;
 
-  summary->optimal_step_size = solution.x;
+  if (!solution.value_is_valid || solution.value > bracket_low.value) {
+    summary->optimal_point = bracket_low;
+  } else {
+    summary->optimal_point = solution;
+  }
+
   summary->success = true;
 }
 
--- internal/ceres/line_search_minimizer.cc
+++ internal/ceres/line_search_minimizer.cc
@@ -63,27 +63,14 @@ namespace ceres {
 namespace internal {
 namespace {
 
-// TODO(sameeragarwal): I think there is a small bug here, in that if
-// the evaluation fails, then the state can contain garbage. Look at
-// this more carefully.
-bool Evaluate(Evaluator* evaluator,
-              const Vector& x,
-              LineSearchMinimizer::State* state,
-              std::string* message) {
-  if (!evaluator->Evaluate(x.data(),
-                           &(state->cost),
-                           NULL,
-                           state->gradient.data(),
-                           NULL)) {
-    *message = "Gradient evaluation failed.";
-    return false;
-  }
-
+bool EvaluateGradientNorms(Evaluator* evaluator,
+                           const Vector& x,
+                           LineSearchMinimizer::State* state,
+                           std::string* message) {
   Vector negative_gradient = -state->gradient;
   Vector projected_gradient_step(x.size());
-  if (!evaluator->Plus(x.data(),
-                       negative_gradient.data(),
-                       projected_gradient_step.data())) {
+  if (!evaluator->Plus(
+          x.data(), negative_gradient.data(), projected_gradient_step.data())) {
     *message = "projected_gradient_step = Plus(x, -gradient) failed.";
     return false;
   }
@@ -116,9 +103,6 @@ void LineSearchMinimizer::Minimize(const Minimizer::Options& options,
   State current_state(num_parameters, num_effective_parameters);
   State previous_state(num_parameters, num_effective_parameters);
 
-  Vector delta(num_effective_parameters);
-  Vector x_plus_delta(num_parameters);
-
   IterationSummary iteration_summary;
   iteration_summary.iteration = 0;
   iteration_summary.step_is_valid = false;
@@ -131,7 +115,18 @@ void LineSearchMinimizer::Minimize(const Minimizer::Options& options,
   iteration_summary.step_solver_time_in_seconds = 0;
 
   // Do initial cost and gradient evaluation.
-  if (!Evaluate(evaluator, x, &current_state, &summary->message)) {
+  if (!evaluator->Evaluate(x.data(),
+                           &(current_state.cost),
+                           NULL,
+                           current_state.gradient.data(),
+                           NULL)) {
+    summary->termination_type = FAILURE;
+    summary->message = "Initial cost and jacobian evaluation failed.";
+    LOG_IF(WARNING, is_not_silent) << "Terminating: " << summary->message;
+    return;
+  }
+
+  if (!EvaluateGradientNorms(evaluator, x, &current_state, &summary->message)) {
     summary->termination_type = FAILURE;
     summary->message = "Initial cost and jacobian evaluation failed. "
         "More details: " + summary->message;
@@ -325,28 +320,43 @@ void LineSearchMinimizer::Minimize(const Minimizer::Options& options,
       break;
     }
 
-    current_state.step_size = line_search_summary.optimal_step_size;
-    delta = current_state.step_size * current_state.search_direction;
+    const FunctionSample& optimal_point = line_search_summary.optimal_point;
+    if (optimal_point.x == 0.0) {
+      summary->message =
+          StringPrintf("Parameter tolerance reached. "
+                       "Relative step_norm: 0.0 <= %e.",
+                       options.parameter_tolerance);
+      summary->termination_type = CONVERGENCE;
+      VLOG_IF(1, is_not_silent) << "Terminating: " << summary->message;
+      return;
+    }
 
+    current_state.step_size = optimal_point.x;
     previous_state = current_state;
     iteration_summary.step_solver_time_in_seconds =
         WallTimeInSeconds() - iteration_start_time;
 
-    const double x_norm = x.norm();
-
-    if (!evaluator->Plus(x.data(), delta.data(), x_plus_delta.data())) {
-      summary->termination_type = FAILURE;
-      summary->message =
-          "x_plus_delta = Plus(x, delta) failed. This should not happen "
-          "as the step was valid when it was selected by the line search.";
-      LOG_IF(WARNING, is_not_silent) << "Terminating: " << summary->message;
-      break;
+    CHECK(optimal_point.value_is_valid);
+    if (optimal_point.gradient_is_valid) {
+      current_state.cost = optimal_point.value;
+      current_state.gradient = optimal_point.vector_gradient;
+    } else {
+      if (!evaluator->Evaluate(optimal_point.vector_x.data(),
+                               &(current_state.cost),
+                               NULL,
+                               current_state.gradient.data(),
+                               NULL)) {
+        summary->termination_type = FAILURE;
+        summary->message = "Cost and jacobian evaluation failed.";
+        LOG_IF(WARNING, is_not_silent) << "Terminating: " << summary->message;
+        return;
+      }
     }
 
-    if (!Evaluate(evaluator,
-                  x_plus_delta,
-                  &current_state,
-                  &summary->message)) {
+    if (!EvaluateGradientNorms(evaluator,
+                               optimal_point.vector_x,
+                               &current_state,
+                               &summary->message)) {
       summary->termination_type = FAILURE;
       summary->message =
           "Step failed to evaluate. This should not happen as the step was "
@@ -357,8 +367,9 @@ void LineSearchMinimizer::Minimize(const Minimizer::Options& options,
     }
 
     // Compute the norm of the step in the ambient space.
-    iteration_summary.step_norm = (x_plus_delta - x).norm();
-    x = x_plus_delta;
+    iteration_summary.step_norm = (optimal_point.vector_x - x).norm();
+    const double x_norm = x.norm();
+    x = optimal_point.vector_x;
 
     iteration_summary.gradient_max_norm = current_state.gradient_max_norm;
     iteration_summary.gradient_norm = sqrt(current_state.gradient_squared_norm);
