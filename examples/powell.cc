0aad590005953062bcb621b97a29c69646d2d2df
Alex Stewart <alexs.mac@gmail.com>
2017-08-13 14:57:41 +0100

    Fix typo in definition of f3 in powell example (x4 -> x3).
    - This was reported as issue #307 by versatran01.
    
    Change-Id: I2c8bbd3466d46b550f81fdadb8b1f4e23b858162

--- examples/powell.cc
+++ examples/powell.cc
@@ -77,10 +77,10 @@ struct F2 {
 
 struct F3 {
   template <typename T> bool operator()(const T* const x2,
-                                        const T* const x4,
+                                        const T* const x3,
                                         T* residual) const {
     // f3 = (x2 - 2 x3)^2
-    residual[0] = (x2[0] - 2.0 * x4[0]) * (x2[0] - 2.0 * x4[0]);
+    residual[0] = (x2[0] - 2.0 * x3[0]) * (x2[0] - 2.0 * x3[0]);
     return true;
   }
 };
