283bcd4da4e530b00729390e96c31ab94db9da31
Keir Mierle <mierle@gmail.com>
2017-10-14 18:02:15 -0700

    DO NOT MERGE: Switch Tiny Solver functions to take Eigen objects
    The current tiny solver function interface takes raw double
    pointers for the parameters, residuals, and jacobian. While
    it avoids having Eigen as part of the public interface, it
    also reduces some safety. This is a trial patch to look at
    what the interface might look like with Eigen types as
    parameters instead.
    
    Change-Id: Ide0fd134cb36f7fccbb225843e7ca1fda15e17dd

--- internal/ceres/tiny_solver_test.cc
+++ internal/ceres/tiny_solver_test.cc
@@ -42,89 +42,76 @@ typedef Eigen::Matrix<double, 2, 1> Vec2;
 typedef Eigen::Matrix<double, 3, 1> Vec3;
 typedef Eigen::VectorXd VecX;
 
-bool EvaluateResidualsAndJacobians(const double* parameters,
-                                   double* residuals,
-                                   double* jacobian) {
-  double x = parameters[0];
-  double y = parameters[1];
-  double z = parameters[2];
+template<typename Parameters, typename Residuals, typename Jacobian>
+bool EvaluateResidualsAndJacobians(const Parameters& parameters,
+                                   Residuals& residuals,
+                                   Jacobian* jacobian) {
+  double x = parameters(0);
+  double y = parameters(1);
+  double z = parameters(2);
 
-  residuals[0] = x + 2*y + 4*z;
-  residuals[1] = y * z;
+  residuals(0) = x + 2*y + 4*z;
+  residuals(1) = y * z;
 
-  if (jacobian) {
-    jacobian[0 * 2 + 0] = 1;
-    jacobian[0 * 2 + 1] = 0;
+  if (jacobian != NULL) {
+    // First column.
+    (*jacobian)(0, 0) = 1;
+    (*jacobian)(1, 0) = 0;
 
-    jacobian[1 * 2 + 0] = 2;
-    jacobian[1 * 2 + 1] = z;
+    // Second column.
+    (*jacobian)(0, 1) = 2;
+    (*jacobian)(1, 1) = z;
 
-    jacobian[2 * 2 + 0] = 4;
-    jacobian[2 * 2 + 1] = y;
+    // Third column.
+    (*jacobian)(0, 2) = 4;
+    (*jacobian)(1, 2) = y;
   }
   return true;
 }
 
 class ExampleStatic {
  public:
-  typedef double Scalar;
-  enum {
-    // Can also be Eigen::Dynamic.
-    NUM_RESIDUALS = 2,
-    NUM_PARAMETERS = 3,
-  };
-  bool operator()(const double* parameters,
-                  double* residuals,
-                  double* jacobian) const {
+  CERES_TINY_SOLVER_CONFIG(double, 2, 3);
+  bool operator()(const Parameters& parameters,
+                  Residuals& residuals,
+                  Jacobian* jacobian) const {
     return EvaluateResidualsAndJacobians(parameters, residuals, jacobian);
   }
 };
 
 class ExampleParametersDynamic {
  public:
-  typedef double Scalar;
-  enum {
-    NUM_RESIDUALS = 2,
-    NUM_PARAMETERS = Eigen::Dynamic,
-  };
+  CERES_TINY_SOLVER_CONFIG(double, 2, Eigen::Dynamic);
 
   int NumParameters() const {
     return 3;
   }
 
-  bool operator()(const double* parameters,
-                  double* residuals,
-                  double* jacobian) const {
+  bool operator()(const Parameters& parameters,
+                  Residuals& residuals,
+                  Jacobian* jacobian) const {
     return EvaluateResidualsAndJacobians(parameters, residuals, jacobian);
   }
 };
 
 class ExampleResidualsDynamic {
  public:
-  typedef double Scalar;
-  enum {
-    NUM_RESIDUALS = Eigen::Dynamic,
-    NUM_PARAMETERS = 3,
-  };
+  CERES_TINY_SOLVER_CONFIG(double, Eigen::Dynamic, 3);
 
   int NumResiduals() const {
     return 2;
   }
 
-  bool operator()(const double* parameters,
-                  double* residuals,
-                  double* jacobian) const {
+  bool operator()(const Parameters& parameters,
+                  Residuals& residuals,
+                  Jacobian* jacobian) const {
     return EvaluateResidualsAndJacobians(parameters, residuals, jacobian);
   }
 };
 
 class ExampleAllDynamic {
  public:
-  typedef double Scalar;
-  enum {
-    NUM_RESIDUALS = Eigen::Dynamic,
-    NUM_PARAMETERS = Eigen::Dynamic,
-  };
+  CERES_TINY_SOLVER_CONFIG(double, Eigen::Dynamic, Eigen::Dynamic);
 
   int NumResiduals() const {
     return 2;
@@ -134,24 +121,31 @@ class ExampleAllDynamic {
     return 3;
   }
 
-  bool operator()(const double* parameters,
-                  double* residuals,
-                  double* jacobian) const {
+  bool operator()(const Parameters& parameters,
+                  Residuals& residuals,
+                  Jacobian* jacobian) const {
     return EvaluateResidualsAndJacobians(parameters, residuals, jacobian);
   }
 };
 
 template <typename Function, typename Vector>
 void TestHelper(const Function& f, const Vector& x0) {
-  Vector x = x0;
-  Vec2 residuals;
-  f(x.data(), residuals.data(), NULL);
+  typedef TinySolver<Function> Solver;
+
+  Solver solver;
+  typename Function::Parameters x = x0;
+  typename Function::Residuals residuals;
+
+  // Needed for dynamic case.
+  residuals.resize(2, 1);
+
+  // Verify that before the solve, residuals are not close to zero.
+  f(x, residuals, NULL);
   EXPECT_GT(residuals.norm(), 1e-10);
 
-  TinySolver<Function> solver;
+  // Verify that after the solve, residual is close to zero.
   solver.Solve(f, &x);
-
-  f(x.data(), residuals.data(), NULL);
+  f(x, residuals, NULL);
   EXPECT_NEAR(0.0, residuals.norm(), 1e-10);
 }
 
