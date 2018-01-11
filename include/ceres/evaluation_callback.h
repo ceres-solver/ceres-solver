9039c8d8afbfc0c030708e7c82ed02b1b604650d
Keir Mierle <mierle@gmail.com>
2018-01-10 17:14:57 -0800

    DO NOT MERGE: Evaluation callback API
    This adds a callback mechanism to for users to get notified just
    before jacobian and residual evaluations. This will enable
    aggressive caching and sharing of compute between cost functions.
    
    Change-Id: I67993726920218edf71ab9ae70c34c204756c71a

---
+++ include/ceres/evaluation_callback.h
@@ -0,0 +1,82 @@
+// Ceres Solver - A fast non-linear least squares minimizer
+// Copyright 2018 Google Inc. All rights reserved.
+// http://ceres-solver.org/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of Google Inc. nor the names of its contributors may be
+//   used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Author: mierle@gmail.com (Keir Mierle)
+//
+// This callback interface allows Ceres to notify you when it is about to
+// evaluate the residuals or jacobians with a new set of parameters. The use
+// case for this is to allow sharing some computation between residual blocks,
+// by doing that computation before Ceres calls CostFunction::Evaluate() on all
+// the residuals. It also enables caching results between a pure residual
+// evaluation and a residual & jacobian evaluation, via the
+// new_evaluation_point argument.
+//
+// One example use is where the cost functions provided to Ceres depend on
+// positions which are computed via IMU integration, where points at the end of
+// a time window depend on all previous points in time. Naive cost function
+// evaluation would have O(n^2) complexity in the number of points along the
+// integration path if there was a Ceres cost function for each point. By
+// pre-computing part of the evalutation inside a PrepareForResidual() or
+// similar callback, that data can get re-used by the cost functions when Ceres
+// calls them as part of normal evaluation. In this example, compute would
+// become O(n) instead of O(n^2).
+//
+// Note that Ceres provides no mechanism to share data other than the callback.
+// Users must provide access to pre-computed shared data to their cost
+// functions behind the scenes; this all happens without Ceres knowing. One
+// approach is to put a pointer to the shared data in each cost function
+// (recommended) or to use a global shared variable (discouraged; bug-prone).
+// As far as Ceres is concerned, it is evaluating cost functions like any
+// other; it just so happens that behind the scenes the cost functions reuse
+// pre-computed data to execute faster.
+
+#ifndef CERES_PUBLIC_EVALUATION_CALLBACK_H_
+#define CERES_PUBLIC_EVALUATION_CALLBACK_H_
+
+namespace ceres {
+
+// Mechanism to get notified by Ceres when it is about to evaluate the cost
+// functions with new parameters.
+class CERES_EXPORT EvaluationCallback {
+ public:
+  virtual ~EvaluationCallback() {}
+
+  // Called before Ceres requests residuals or jacobians for a given setting of
+  // the parameters. User parameters (the double* values provided to the cost
+  // functions) are fixed until the next call to PrepareForEvaluation() If
+  // new_evaluation_point == true, then this is a new point that is different
+  // from the last evaluated point. Otherwise, it is the same point that was
+  // evaluated previously (either jacobian or residual) and the user can use
+  // cached results from previous evaluations.
+  virtual void PrepareForEvaluation(bool jacobians,
+                                    bool new_evaluation_point) = 0;
+};
+
+}  // namespace ceres
+
+#endif  // CERES_PUBLIC_EVALUATION_CALLBACK_H_
--- include/ceres/solver.h
+++ include/ceres/solver.h
@@ -39,6 +39,7 @@
 #include "ceres/internal/macros.h"
 #include "ceres/internal/port.h"
 #include "ceres/iteration_callback.h"
+#include "ceres/evaluation_callback.h"
 #include "ceres/ordered_groups.h"
 #include "ceres/types.h"
 
@@ -136,6 +137,7 @@ class CERES_EXPORT Solver {
       gradient_check_relative_precision = 1e-8;
       gradient_check_numeric_derivative_relative_step_size = 1e-6;
       update_state_every_iteration = false;
+      evaluation_callback = NULL;
     }
 
     // Returns true if the options struct has a valid
@@ -742,6 +744,10 @@ class CERES_EXPORT Solver {
     // Minimizer terminates. This is useful if, for example, the user
     // wishes to visualize the state of the optimization every
     // iteration.
+    //
+    // Note: This option is ignored if evaluation_callback != NULL. In that
+    // case, user parameter locks are updated for every residual and jacobian
+    // evaluation.
     bool update_state_every_iteration;
 
     // Callbacks that are executed at the end of each iteration of the
@@ -758,8 +764,24 @@ class CERES_EXPORT Solver {
     // to the update parameter blocks when his/her callbacks are
     // executed, then set update_state_every_iteration to true.
     //
+    // Note: If evaluation_callback != NULL, then the user points will be
+    // unconditionally updated for every residual and jacobian evaluation.
+    //
     // The solver does NOT take ownership of these pointers.
     std::vector<IterationCallback*> callbacks;
+
+    // If non-empty, get notified when Ceres is about to evaluate the
+    // residuals. This is incompatible with some solver options (e.g. inner
+    // iterations); however, it enables sharing computation between residuals,
+    // which in some cases is important for efficient evaluation.
+    //
+    // Note: For the evaluation callback to function properly, Ceres must
+    // update the data in user's parameter blocks for every cost function
+    // evaluation (even for unsuccessful steps). This happens regardless of
+    // update_state_every_iteration.
+    //
+    // The solver does NOT take ownership of the pointer.
+    EvaluationCallback* evaluation_callback;
   };
 
   struct CERES_EXPORT Summary {
