18d7daedbcc09b7e749cba1793a5720dc400bf9d
Keir Mierle <mierle@gmail.com>
2018-01-10 17:14:57 -0800

    Evaluation callback API
    This adds a callback mechanism to for users to get notified just
    before jacobian and residual evaluations. This will enable
    aggressive caching and sharing of compute between cost functions.
    
    Change-Id: I67993726920218edf71ab9ae70c34c204756c71a

--- internal/ceres/line_search_preprocessor.cc
+++ internal/ceres/line_search_preprocessor.cc
@@ -60,6 +60,7 @@ bool SetupEvaluator(PreprocessedProblem* pp) {
   pp->evaluator_options.num_eliminate_blocks = 0;
   pp->evaluator_options.num_threads = pp->options.num_threads;
   pp->evaluator_options.context = pp->problem->context();
+  pp->evaluator_options.evaluation_callback = pp->options.evaluation_callback;
   pp->evaluator.reset(Evaluator::Create(pp->evaluator_options,
                                         pp->reduced_program.get(),
                                         &pp->error));
