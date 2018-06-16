990777eff91e1f2bea1b62399478fea70db0ceaf
Alex Stewart <alexs.mac@gmail.com>
2018-06-16 20:49:17 +0100

    Refactor Ceres threading option configuration.
    - Previously we had separate variables for each of the threading
      backends, each of which were made mutually exclusive via
      cmake_dependent_option().  This has unfortunate side-effects when
      trying to disable options if they are not currently enabled, in which
      case they are not defined.
    - As all the threading options are mutually exclusive, this replaces
      all threading option variables with a single variable: CERES_THREADS,
      which is constrained to take the value of only the available threading
      backends.
    
    Change-Id: I0822eefbac9a30772907b7732add365b37cc8ca0

---
+++ cmake/CeresThreadingOptions.cmake
@@ -0,0 +1,90 @@
+# Ceres Solver - A fast non-linear least squares minimizer
+# Copyright 2018 Google Inc. All rights reserved.
+# http://ceres-solver.org/
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of Google Inc. nor the names of its contributors may be
+#   used to endorse or promote products derived from this software without
+#   specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: alexs.mac@gmail.com (Alex Stewart)
+
+# Ordered by expected preference.
+set(CERES_THREADING_OPTIONS "CXX11_THREADS;OPENMP;TBB;NO_THREADS")
+
+function(find_available_ceres_threading_options CERES_AVAILABLE_THREADING_OPTIONS_VAR)
+  set(CERES_AVAILABLE_THREADING_OPTIONS ${CERES_THREADING_OPTIONS})
+  # Remove any threading options for which the dependencies are not available.
+  find_package(OpenMP QUIET)
+  if (NOT OPENMP_FOUND)
+    list(REMOVE_ITEM CERES_AVAILABLE_THREADING_OPTIONS "OPENMP")
+  endif()
+  find_package(TBB QUIET)
+  if (NOT TBB_FOUND)
+    list(REMOVE_ITEM CERES_AVAILABLE_THREADING_OPTIONS "TBB")
+  endif()
+  if (NOT CERES_AVAILABLE_THREADING_OPTIONS)
+    # At least NO_THREADS should never be removed.
+    message(FATAL_ERROR "Ceres bug: Removed all threading options.")
+  endif()
+  set(${CERES_AVAILABLE_THREADING_OPTIONS_VAR}
+    ${CERES_AVAILABLE_THREADING_OPTIONS} PARENT_SCOPE)
+endfunction()
+
+macro(set_ceres_threading_option_to_cxx11_threads)
+  list(APPEND CERES_COMPILE_OPTIONS CERES_USE_CXX11_THREADS)
+endmacro()
+
+macro(set_ceres_threading_option_to_openmp)
+  find_package(OpenMP REQUIRED)
+  list(APPEND CERES_COMPILE_OPTIONS CERES_USE_OPENMP)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+endmacro()
+
+macro(set_ceres_threading_option_to_tbb)
+  find_package(TBB REQUIRED)
+  list(APPEND CERES_COMPILE_OPTIONS CERES_USE_TBB)
+  include_directories(${TBB_INCLUDE_DIRS})
+endmacro()
+
+macro(set_ceres_threading_option_to_no_threads)
+  list(APPEND CERES_COMPILE_OPTIONS CERES_NO_THREADS)
+endmacro()
+
+macro(set_ceres_threading_option CERES_THREADING_OPTION_TO_SET)
+  if ("${CERES_THREADING_OPTION_TO_SET}" STREQUAL "CXX11_THREADS")
+    set_ceres_threading_option_to_cxx11_threads()
+  elseif ("${CERES_THREADING_OPTION_TO_SET}" STREQUAL "OPENMP")
+    set_ceres_threading_option_to_openmp()
+  elseif ("${CERES_THREADING_OPTION_TO_SET}" STREQUAL "TBB")
+    set_ceres_threading_option_to_tbb()
+  elseif ("${CERES_THREADING_OPTION_TO_SET}" STREQUAL "NO_THREADS")
+    set_ceres_threading_option_to_no_threads()
+  else()
+    find_available_ceres_threading_options(_AVAILABLE_THREADING_OPTIONS)
+    message(FATAL_ERROR "Unknown threading option specified: "
+      "'${CERES_THREADING_OPTION_TO_SET}'. Available threading options for "
+      "this platform are: '${_AVAILABLE_THREADING_OPTIONS}'")
+  endif()
+  message("-- Using Ceres threading option: ${CERES_THREADING_OPTION_TO_SET}")
+endmacro()
