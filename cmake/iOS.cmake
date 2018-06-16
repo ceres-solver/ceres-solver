7582834a7ec2838163a215372cfe72484a3a47a2
Alex Stewart <alexs.mac@gmail.com>
2018-06-16 19:32:19 +0100

    Do not bypass default CMake compiler checks on iOS.
    - This should no longer be required on newer (3.5+) CMake versions and
      breaks the detection of supplementary compiler feature information
      such as the population of CMAKE_CXX_COMPILE_FEATURES which is required
      in order to compile Ceres.
    
    Change-Id: Ibceab545925aa1e829320efa03fb74ef149cbb8b

--- cmake/iOS.cmake
+++ cmake/iOS.cmake
@@ -224,12 +224,6 @@ set(CMAKE_OSX_DEPLOYMENT_TARGET "" CACHE STRING
 # Set the architectures for which to build.
 set(CMAKE_OSX_ARCHITECTURES ${IOS_ARCH} CACHE STRING "Build architecture for iOS")
 
-# Skip the platform compiler checks for cross compiling.
-set(CMAKE_CXX_COMPILER_FORCED TRUE)
-set(CMAKE_CXX_COMPILER_WORKS TRUE)
-set(CMAKE_C_COMPILER_FORCED TRUE)
-set(CMAKE_C_COMPILER_WORKS TRUE)
-
 # All iOS/Darwin specific settings - some may be redundant.
 set(CMAKE_SHARED_LIBRARY_PREFIX "lib")
 set(CMAKE_SHARED_LIBRARY_SUFFIX ".dylib")
