e6f7a75fdc94c9d1f4cfa0fe0ee4cc88a3fccf6e
Alex Stewart <alexs.mac@gmail.com>
2018-08-07 12:06:39 +0100

    Fix brew installation failing if formula is already installed.
    Change-Id: I8b297a7b8ae1cd2e3a34e38f52121f420d5b7651

--- travis/install_travis_osx_deps.sh
+++ travis/install_travis_osx_deps.sh
@@ -3,4 +3,17 @@
 # Stop processing on any error.
 set -e
 
-brew install cmake glog gflags eigen suite-sparse
+function install_if_not_installed() {
+  declare -r formula="$1"
+  if [[ $(brew list ${formula} &>/dev/null; echo $?) -ne 0 ]]; then
+    brew install ${formula}
+  else
+    echo "$0 - ${formula} is already installed."
+  fi
+}
+
+install_if_not_installed cmake
+install_if_not_installed glog
+install_if_not_installed gflags
+install_if_not_installed eigen
+install_if_not_installed suite-sparse
