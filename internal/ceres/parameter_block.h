14aedee82d81146a579b7059ddd9fe004a8fbbc1
Sameer Agarwal <sameeragarwal@google.com>
2019-07-11 16:30:59 +0200

    Allow LocalParameterizations to have zero local size.
    https://github.com/ceres-solver/ceres-solver/issues/347
    
    Change-Id: I554a2acc420f5dd9d0cc7f97b691877eb057b2c0

--- internal/ceres/parameter_block.h
+++ internal/ceres/parameter_block.h
@@ -38,6 +38,7 @@
 #include <memory>
 #include <string>
 #include <unordered_set>
+
 #include "ceres/array_utils.h"
 #include "ceres/internal/eigen.h"
 #include "ceres/internal/port.h"
@@ -190,9 +191,9 @@ class ParameterBlock {
         << "size of " << new_parameterization->GlobalSize() << ". Did you "
         << "accidentally use the wrong parameter block or parameterization?";
 
-    CHECK_GT(new_parameterization->LocalSize(), 0)
+    CHECK_GE(new_parameterization->LocalSize(), 0)
         << "Invalid parameterization. Parameterizations must have a "
-        << "positive dimensional tangent space.";
+        << "non-negative dimensional tangent space.";
 
     local_parameterization_ = new_parameterization;
     local_parameterization_jacobian_.reset(
