1ce8d2b855a8c8b4176e4338a217cdca5b641f4a
Darius Rueckert <darius.rueckert@fau.de>
2019-10-02 12:45:59 +0200

    Code Generation Part 1: Expressions
    This patch adds the 'Expression' class, which is a fundamental
    building block of automatic code generation. The expressions can
    be used as scalar types for cost functors as well as Jets.
    Dynamic branching is not yet supported.
    
    Change-Id: I8c61bee5c307e0eec20fd39382683ea90f720dff

---
+++ include/ceres/internal/expression_graph.h
@@ -0,0 +1,84 @@
+// Ceres Solver - A fast non-linear least squares minimizer
+// Copyright 2019 Google Inc. All rights reserved.
+// http://code.google.com/p/ceres-solver/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of Google Inc. nor the names of its contributors may be
+//   used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Author: darius.rueckert@fau.de (Darius Rueckert)
+
+#ifndef CERES_PUBLIC_EXPRESSION_TREE_H_
+#define CERES_PUBLIC_EXPRESSION_TREE_H_
+
+#include <vector>
+
+#include "expression.h"
+
+namespace ceres {
+namespace internal {
+
+// An undirected, non-cyclic, unconnected graph containing all expressions of a
+// program.
+//
+// The expression graph is stored linear in the data_ array. The order is
+// identical to the execution order. Each expression can have multiple children
+// and multiple parents.
+// A is child of B     <=>  B has A as a parameter    <=> B.DirectlyDependsOn(A)
+// A is parent of B    <=>  A has B as a parameter    <=> A.DirectlyDependsOn(B)
+class ExpressionGraph {
+ public:
+  // Creates an expression and adds it to data_.
+  // The returned reference will be invalid after this function is called again.
+  Expression& MakeExpression(ExpressionType type);
+
+  // Checks if A depends on B.
+  // -> B is a descendant of A
+  bool DependsOn(ExpressionId A, ExpressionId B) const;
+
+  Expression& get(ExpressionId id) { return data_[id]; }
+  const Expression& get(ExpressionId id) const { return data_[id]; }
+
+ private:
+  std::vector<Expression> data_;
+};
+
+// After calling this method, all operations on 'ExpressionRef' objects will be
+// recorded into an internal array. You can obtain this array by calling
+// StopRecordingExpressions.
+//
+// Performing expression operations before calling StartRecordingExpressions is
+// an error.
+void StartRecordingExpressions();
+
+// Stops recording and returns all expressions that have been executed since the
+// call to StartRecordingExpressions.
+ExpressionGraph StopRecordingExpressions();
+
+// Returns a pointer to the active expression tree.
+// Normal users should not use this functions.
+ExpressionGraph* GetCurrentExpressionTree();
+
+}  // namespace internal
+}  // namespace ceres
+#endif
