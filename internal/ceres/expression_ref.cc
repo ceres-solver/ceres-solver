03997333f61266405f6e817416b2ee3d73477c4c
Darius Rueckert <darius.rueckert@fau.de>
2019-10-21 11:01:08 +0800

    Autodiff Codegen Part 2: Conditionals
    - Expression types for if/else blocks
    - CERES_IF/ELSE macros
    - Multiple assignment to the same variable
    
    Change-Id: If529516f243f31823d1ef7b8827bb6f2390e418d

--- internal/ceres/expression_ref.cc
+++ internal/ceres/expression_ref.cc
@@ -29,8 +29,7 @@
 // Author: darius.rueckert@fau.de (Darius Rueckert)
 
 #include "ceres/internal/expression_ref.h"
-#include "assert.h"
-#include "ceres/internal/expression.h"
+#include "glog/logging.h"
 
 namespace ceres {
 namespace internal {
@@ -41,11 +40,24 @@ ExpressionRef ExpressionRef::Create(ExpressionId id) {
   return ref;
 }
 
-std::string ExpressionRef::ToString() const { return std::to_string(id); }
-
 ExpressionRef::ExpressionRef(double compile_time_constant) {
-  (*this) = ExpressionRef::Create(
-      Expression::CreateCompileTimeConstant(compile_time_constant));
+  id = Expression::CreateCompileTimeConstant(compile_time_constant);
+}
+
+ExpressionRef& ExpressionRef::operator=(const ExpressionRef& other) {
+  // Assigning an uninitialized variable to another variable is an error.
+  CHECK(other.IsInitialized()) << "Uninitialized Assignment.";
+
+  if (IsInitialized()) {
+    // Create assignment from other -> this
+    Expression::CreateAssignment(id, other.id);
+  } else {
+    // Special case: "this" expressionref is invalid
+    //    -> Skip assignment
+    //    -> Let "this" point to the same variable as other
+    id = other.id;
+  }
+  return *this;
 }
 
 // Compound operators
