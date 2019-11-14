2b494cfb3696360e306e219beaa15d0bcddfeb21
Alex Stewart <alexs.mac@gmail.com>
2019-11-14 20:39:30 +0000

    Update Travis CI to Bionic & Xcode 11.2
    - Fix Homebrew Ruby errors by forcing brew update before install.
    - Remove use of custom Eigen 3.3.4 on Linux in favour of default Bionic
      3.3.4 package.
    - Update Android NDK version to R20b and build for arm64-v8.
    - Fix erroneous string -> STRING in iOS toolchain file.
    
    Change-Id: I041c2a08b92b8307c12bd8967159beb6984e3c65

--- travis/install_travis_osx_deps.sh
+++ travis/install_travis_osx_deps.sh
@@ -12,6 +12,12 @@ function install_if_not_installed() {
   fi
 }
 
+# Manually trigger an update prior to installing packages to avoid Ruby
+# version related errors as per [1].
+#
+# [1]: https://github.com/travis-ci/travis-ci/issues/8552
+brew update
+
 install_if_not_installed cmake
 install_if_not_installed glog
 install_if_not_installed gflags
