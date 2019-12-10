26f78ccd7d6a5fdb8d77f905c58a48051ce442ae
Darius Rueckert <darius.rueckert@fau.de>
2019-12-10 16:39:08 +0100

    AutoDiff Code Generation - CMake Integration
    This patch integrates the code generation module into the build
    system. All depenendcies are tracked through CMake targets.
    Modifying the cost functor will automatically trigger code
    re-generation.
    
    All this functionality is defined in the CMake function
    CERES_COST_FUNCTOR in CeresCodeGeneration.cmake. A usage
    example is included in examples/CMakeLists.txt.
    
    Change-Id: I23b8b6698d1ea51cf3d788a47afcf39f8c5ce327

---
+++ examples/helloworld_codegen.cc
@@ -0,0 +1,83 @@
+// Ceres Solver - A fast non-linear least squares minimizer
+// Copyright 2019 Google Inc. All rights reserved.
+// http://code.google.com/p/ceres-solver/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of Google Inc. nor the names of its contributors may be
+//   used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Author: darius.rueckert@fau.de (Darius Rueckert)
+//
+// A simple example showing how to generate code for a cost functor
+//
+// We recommend to use the CMake integration instead of using
+// GenerateCodeForFunctor directly.
+//
+#include "ceres/autodiff_cost_function.h"
+#include "ceres/codegen/autodiff.h"
+#include "ceres/sized_cost_function.h"
+
+#include "ceres/ceres.h"
+#include "glog/logging.h"
+#include "square_functor.h"
+
+using ceres::AutoDiffCostFunction;
+using ceres::CostFunction;
+using ceres::Problem;
+using ceres::SizedCostFunction;
+using ceres::Solve;
+using ceres::Solver;
+
+class SquareCostFunction : public SizedCostFunction<1, 1> {
+ public:
+  virtual ~SquareCostFunction() {}
+// Inject the generated code into the cost function
+#include "square_cost_function.h"
+};
+
+int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+
+  // The variable to solve for with its initial value. It will be
+  // mutated in place by the solver.
+  double x = 0.5;
+  const double initial_x = x;
+
+  // Build the problem.
+  Problem problem;
+
+  // Set up the only cost function (also known as residual). This uses
+  // auto-differentiation to obtain the derivative (jacobian).
+  CostFunction* cost_function = new SquareCostFunction;
+  problem.AddResidualBlock(cost_function, NULL, &x);
+
+  // Run the solver!
+  Solver::Options options;
+  options.minimizer_progress_to_stdout = true;
+  Solver::Summary summary;
+  Solve(options, &problem, &summary);
+
+  std::cout << summary.BriefReport() << "\n";
+  std::cout << "x : " << initial_x << " -> " << x << "\n";
+  return 0;
+}
