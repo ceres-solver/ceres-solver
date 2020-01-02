57bcc251e33c83c4f802702b9c9dcf15cc5e5108
Johannes Beck <Jodebo_Beck@gmx.de>
2020-01-02 19:17:31 +0100

    Fix windows MSVC build.
    This CL improves the build experience with MSVC:
    - It adds the build flag '/bigobj' otherwise the build of
      the unit test fails.
    - It adds the flag '/wd4267' to suppress signed / unsigned int
      conversions (size_t to int).
    - It removes the use of std::aligned_storage from FixedArray.
      This has been done from the Abseil Team and is in the
      absl::FixedArray. Those changes has been ported to ceres.
      This fixes the alignemnt for older MSVC versions due to a
      bug in the implementation of std::aligned_storage, and
      prevents the use of the macro '_ENABLE_EXTENDED_ALIGNED_STORAGE'
      for newer MSVC versions, which is problematic as it could affect
      user code.
    - Fix of the fixed array unit test. Due to the use of std::tuple
      instead of absl::tuple in ceres::internal::FixedArray, the
      unit test needs to reflect that change as well.
    - Replaces 'add_definitions' with 'add_compile_options' for
      compiler flags as suggested by the cmake documentation.
    
    Change-Id: I63f08cd6c0a8db8c9931289b909b4deafd75b039

--- include/ceres/internal/fixed_array.h
+++ include/ceres/internal/fixed_array.h
@@ -163,8 +163,7 @@ class FixedArray {
     CopyRange(storage_.alloc(), storage_.begin(), first, last);
   }
 
-  // Releases any resources.
-  ~FixedArray() {
+  ~FixedArray() noexcept {
     for (auto* cur = storage_.begin(); cur != storage_.end(); ++cur) {
       AllocatorTraits::destroy(storage_.alloc(), cur);
     }
@@ -358,7 +357,7 @@ class FixedArray {
   //     error: call to int __builtin___sprintf_chk(etc...)
   //     will always overflow destination buffer [-Werror]
   //
-  template <typename OuterT = value_type,
+  template <typename OuterT,
             typename InnerT = typename std::remove_extent<OuterT>::type,
             size_t InnerN = std::extent<OuterT>::value>
   struct StorageElementWrapper {
@@ -369,9 +368,6 @@ class FixedArray {
       typename std::conditional<std::is_array<value_type>::value,
                                 StorageElementWrapper<value_type>,
                                 value_type>::type;
-  using StorageElementBuffer =
-      typename std::aligned_storage<sizeof(StorageElement),
-                                    alignof(StorageElement)>::type;
 
   static pointer AsValueType(pointer ptr) { return ptr; }
   static pointer AsValueType(StorageElementWrapper<value_type>* ptr) {
@@ -381,25 +377,25 @@ class FixedArray {
   static_assert(sizeof(StorageElement) == sizeof(value_type), "");
   static_assert(alignof(StorageElement) == alignof(value_type), "");
 
-  struct NonEmptyInlinedStorage {
-    StorageElement* data() {
-      return reinterpret_cast<StorageElement*>(inlined_storage_.data());
-    }
+  class NonEmptyInlinedStorage {
+   public:
+    StorageElement* data() { return reinterpret_cast<StorageElement*>(buff_); }
+    void AnnotateConstruct(size_type) {}
+    void AnnotateDestruct(size_type) {}
 
     // #ifdef ADDRESS_SANITIZER
     //     void* RedzoneBegin() { return &redzone_begin_; }
     //     void* RedzoneEnd() { return &redzone_end_ + 1; }
     // #endif  // ADDRESS_SANITIZER
 
-    void AnnotateConstruct(size_type) {}
-    void AnnotateDestruct(size_type) {}
-
+   private:
     // ADDRESS_SANITIZER_REDZONE(redzone_begin_);
-    std::array<StorageElementBuffer, inline_elements> inlined_storage_;
+    alignas(StorageElement) char buff_[sizeof(StorageElement[inline_elements])];
     // ADDRESS_SANITIZER_REDZONE(redzone_end_);
   };
 
-  struct EmptyInlinedStorage {
+  class EmptyInlinedStorage {
+   public:
     StorageElement* data() { return nullptr; }
     void AnnotateConstruct(size_type) {}
     void AnnotateDestruct(size_type) {}
@@ -459,6 +455,13 @@ class FixedArray {
 
   Storage storage_;
 };
+
+template <typename T, size_t N, typename A>
+constexpr size_t FixedArray<T, N, A>::kInlineBytesDefault;
+
+template <typename T, size_t N, typename A>
+constexpr typename FixedArray<T, N, A>::size_type
+    FixedArray<T, N, A>::inline_elements;
 
 }  // namespace internal
 }  // namespace ceres
