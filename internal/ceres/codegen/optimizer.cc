4410689c963aae784936626fd13d9219eddfca6a
Darius Rueckert <darius.rueckert@fau.de>
2020-02-12 09:21:42 +0100

    Codegen Optimizer API
    This patch adds the classes Optimizer and OptimizationPass, which
    define the core API of the codegen optimization.
    
    A single (trivial) optimization is added that removes NOP
    expressions from a graph.
    
    Change-Id: I0430d6ffc61a474f997475ff787a81a9c69cfe23

---
+++ internal/ceres/codegen/optimizer.cc
@@ -0,0 +1,62 @@
+// Ceres Solver - A fast non-linear least squares minimizer
+// Copyright 2019 Google Inc. All rights reserved.
+// http://code.google.com/p/ceres-solver/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of Google Inc. nor the names of its contributors may be
+//   used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Author: darius.rueckert@fau.de (Darius Rueckert)
+
+#include "ceres/codegen/internal/optimizer.h"
+
+#include "ceres/codegen/internal/eliminate_nops.h"
+#include "glog/logging.h"
+namespace ceres {
+namespace internal {
+
+OptimizeExpressionGraphSummary OptimizeExpressionGraph(
+    const OptimizeExpressionGraphOptions& options, ExpressionGraph* graph) {
+  OptimizeExpressionGraphSummary summary;
+  int iteration = 0;
+  for (; iteration < options.max_num_iterations; ++iteration) {
+    bool changed = false;
+
+    if (options.eliminate_nops) {
+      auto pass_summary = EliminateNops(graph);
+      changed |= pass_summary.expression_graph_changed;
+      summary.summaries.push_back(pass_summary);
+    }
+
+    if (!changed) {
+      iteration++;
+      break;
+    }
+  }
+  summary.num_iterations = iteration;
+
+  return summary;
+}
+
+}  // namespace internal
+}  // namespace ceres
