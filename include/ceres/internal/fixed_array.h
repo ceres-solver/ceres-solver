0df94e632a4d17dc6d187ec1ff21bf012157e358
Janos95 <janos.meny@googlemail.com>
2020-02-23 00:14:07 +0100

    fixed array: dont use depr member types of std allocator

--- include/ceres/internal/fixed_array.h
+++ include/ceres/internal/fixed_array.h
@@ -106,10 +106,10 @@ class FixedArray {
  public:
   using allocator_type = typename AllocatorTraits::allocator_type;
   using value_type = typename allocator_type::value_type;
-  using pointer = typename allocator_type::pointer;
-  using const_pointer = typename allocator_type::const_pointer;
-  using reference = typename allocator_type::reference;
-  using const_reference = typename allocator_type::const_reference;
+  using pointer = T*;
+  using const_pointer = const T*;
+  using reference = T&;
+  using const_reference = const T&;
   using size_type = typename allocator_type::size_type;
   using difference_type = typename allocator_type::difference_type;
   using iterator = pointer;
