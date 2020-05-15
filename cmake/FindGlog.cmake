27b717951b58c134b3a5a9f664a66c7480364d6c
Alastair Harrison <aharrison24@gmail.com>
2020-05-15 10:10:12 +0100

    Respect FIND_QUIETLY flag in cmake config file
    Ensure that Ceres does not print any log messages when somebody has
    used 'find_package(Ceres QUIET)' in their CMake project.
    
    Change-Id: Id6b68859cc8a5857f3fa78f29736cb82fd5a0943

--- cmake/FindGlog.cmake
+++ cmake/FindGlog.cmake
@@ -109,6 +109,16 @@ macro(GLOG_REPORT_NOT_FOUND REASON_MSG)
   return()
 endmacro(GLOG_REPORT_NOT_FOUND)
 
+# glog_message([mode] "message text")
+#
+# Wraps the standard cmake 'message' command, but suppresses output
+# if the QUIET flag was passed to the find_package(Glog ...) call.
+function(GLOG_MESSAGE)
+  if (NOT Glog_FIND_QUIETLY)
+    message(${ARGN})
+  endif()
+endfunction()
+
 # Protect against any alternative find_package scripts for this library having
 # been called previously (in a client project) which set GLOG_FOUND, but not
 # the other variables we require / set here which could cause the search logic
@@ -123,8 +133,8 @@ unset(GLOG_FOUND)
 if (NOT DEFINED GLOG_PREFER_EXPORTED_GLOG_CMAKE_CONFIGURATION
     AND NOT GLOG_INCLUDE_DIR_HINTS
     AND NOT GLOG_LIBRARY_DIR_HINTS)
-  message(STATUS "No preference for use of exported glog CMake configuration "
-    "set, and no hints for include/library directories provided. "
+  glog_message(STATUS "No preference for use of exported glog CMake "
+    "configuration set, and no hints for include/library directories provided. "
     "Defaulting to preferring an installed/exported glog CMake configuration "
     "if available.")
   set(GLOG_PREFER_EXPORTED_GLOG_CMAKE_CONFIGURATION TRUE)
@@ -144,7 +154,7 @@ if (CMAKE_SYSTEM_NAME MATCHES "Darwin")
     execute_process(COMMAND ${HOMEBREW_EXECUTABLE} --prefix
       OUTPUT_VARIABLE HOMEBREW_INSTALL_PREFIX
       OUTPUT_STRIP_TRAILING_WHITESPACE)
-    message(STATUS "Detected Homebrew with install prefix: "
+    glog_message(STATUS "Detected Homebrew with install prefix: "
       "${HOMEBREW_INSTALL_PREFIX}, adding to CMake search paths.")
     list(APPEND GLOG_INCLUDE_DIR_HINTS "${HOMEBREW_INSTALL_PREFIX}/include")
     list(APPEND GLOG_LIBRARY_DIR_HINTS "${HOMEBREW_INSTALL_PREFIX}/lib")
@@ -202,11 +212,11 @@ if (GLOG_PREFER_EXPORTED_GLOG_CMAKE_CONFIGURATION)
                     NO_CMAKE_PACKAGE_REGISTRY
                     NO_CMAKE_BUILDS_PATH)
   if (glog_FOUND)
-    message(STATUS "Found installed version of glog: ${glog_DIR}")
+    glog_message(STATUS "Found installed version of glog: ${glog_DIR}")
   else()
     # Failed to find an installed version of glog, repeat search allowing
     # exported build directories.
-    message(STATUS "Failed to find installed glog CMake configuration, "
+    glog_message(STATUS "Failed to find installed glog CMake configuration, "
       "searching for glog build directories exported with CMake.")
     # Again pass NO_CMAKE_BUILDS_PATH, as we know that glog is exported and
     # do not want to treat projects built with the CMake GUI preferentially.
@@ -215,21 +225,22 @@ if (GLOG_PREFER_EXPORTED_GLOG_CMAKE_CONFIGURATION)
                       NO_MODULE
                       NO_CMAKE_BUILDS_PATH)
     if (glog_FOUND)
-      message(STATUS "Found exported glog build directory: ${glog_DIR}")
+      glog_message(STATUS "Found exported glog build directory: ${glog_DIR}")
     endif(glog_FOUND)
   endif(glog_FOUND)
 
   set(FOUND_INSTALLED_GLOG_CMAKE_CONFIGURATION ${glog_FOUND})
 
   if (FOUND_INSTALLED_GLOG_CMAKE_CONFIGURATION)
-    message(STATUS "Detected glog version: ${glog_VERSION}")
+    glog_message(STATUS "Detected glog version: ${glog_VERSION}")
     set(GLOG_FOUND ${glog_FOUND})
     # glog wraps the include directories into the exported glog::glog target.
     set(GLOG_INCLUDE_DIR "")
     set(GLOG_LIBRARY glog::glog)
   else (FOUND_INSTALLED_GLOG_CMAKE_CONFIGURATION)
-    message(STATUS "Failed to find an installed/exported CMake configuration "
-      "for glog, will perform search for installed glog components.")
+    glog_message(STATUS "Failed to find an installed/exported CMake "
+      "configuration for glog, will perform search for installed glog "
+      "components.")
   endif (FOUND_INSTALLED_GLOG_CMAKE_CONFIGURATION)
 endif(GLOG_PREFER_EXPORTED_GLOG_CMAKE_CONFIGURATION)
 
