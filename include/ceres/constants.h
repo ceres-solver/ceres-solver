c9f11bf674dcef55ab350bb5aba7f40708e2da6e
hs293go <hellston20a@gmail.com>
2021-11-23 15:39:14 -0500

    Add generalized Euler Angle conversions
    Conversions function include Euler Angles to / from Rotation Matrices
    and Quaternions. They are generalized for any Euler convention that can
    be specified in the arguments. Algorithm is from "Euler angle
    conversion", Ken Shoemake, Graphics Gems IV
    
    Change-Id: I7f9ddc0b8d686efca16299d2ba374295744376ce

---
+++ include/ceres/constants.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 hs293go
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#ifndef CERES_PUBLIC_CONSTANTS_H_
+#define CERES_PUBLIC_CONSTANTS_H_
+
+namespace ceres {
+namespace constants {
+template <typename T>
+inline constexpr T pi_v(3.141592653589793238462643383279502884);
+}
+}  // namespace ceres
+
+#endif  // CERES_PUBLIC_CONSTANTS_H_
