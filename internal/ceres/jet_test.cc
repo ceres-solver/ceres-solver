543dbd42c8e3ebdf327d004f8af64c8f5e3b4df4
Sameer Agarwal <sameeragarwal@google.com>
2021-11-30 11:16:06 -0800

    Use standard c++ types in jet_test.cc
    Change-Id: I8fb91bf04626eba1ceda676dc6564b9798d6a4a9

--- internal/ceres/jet_test.cc
+++ internal/ceres/jet_test.cc
@@ -1402,17 +1402,15 @@ TEST(Jet, nested3x) {
 
 #if GTEST_HAS_TYPED_TEST
 
-using Types = testing::Types<short,
-                             unsigned short,
-                             int,
-                             unsigned int,
-                             long,
-                             unsigned long,
-                             long long,
-                             unsigned long long,
+using Types = testing::Types<std::int16_t,
+                             std::uint16_t,
+                             std::int32_t,
+                             std::uint32_t,
+                             std::int64_t,
+                             std::uint64_t,
                              float,
                              double,
-                             long double>;
+							 long double>;
 
 template <typename T>
 class JetTest : public testing::Test {};
