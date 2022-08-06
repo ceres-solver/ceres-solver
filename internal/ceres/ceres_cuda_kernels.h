f8c2a07983c5730a94b5bd261a4a07e0136b5830
Joydeep Biswas <joydeepb@cs.utexas.edu>
2022-08-05 20:48:17 -0500

    CUDA Cleanup
    * Renamed several interfaces to CudaBuffer for clarity and consistency.
    * Added unit tests for custom Cuda kernels.
    * Set specific CUDA architectures if the CMake version supports it.
    
    Change-Id: I269fb1089b80b25e17bca772ef8d70e7894214b8

--- internal/ceres/ceres_cuda_kernels.h
+++ internal/ceres/ceres_cuda_kernels.h
@@ -62,6 +62,13 @@ void CudaSetZeroFP64(double* output, const int size, cudaStream_t stream);
 // double (FP64). Both arrays must already be on GPU memory.
 void CudaDsxpy(double* x, float* y, const int size, cudaStream_t stream);
 
+// Compute y[i] = y[i] + d[i]^2 x[i]. All arrays must already be on GPU memory.
+void CudaDtDxpy(double* y,
+                const double* D,
+                const double* x,
+                const int size,
+                cudaStream_t stream);
+
 }  // namespace ceres::internal
 
 #endif  // CERES_NO_CUDA
