513095e72903a0740a2d0adea2813d6388a99875
Joydeep Biswas <joydeepb@cs.utexas.edu>
2022-08-16 00:16:24 -0500

    Fixed a few more missing CERES_NO_CUDA guards
    Change-Id: I65c1851085f816794079ef56de0426fcdd055c87

--- internal/ceres/dense_cholesky_test.cc
+++ internal/ceres/dense_cholesky_test.cc
@@ -75,9 +75,11 @@ TEST_P(DenseCholeskyTest, FactorAndSolve) {
 
   LinearSolver::Options options;
   ContextImpl context;
+#ifndef CERES_NO_CUDA
   options.context = &context;
   std::string error;
   CHECK(context.InitCUDA(&error)) << error;
+#endif  // CERES_NO_CUDA
   options.dense_linear_algebra_library_type = ::testing::get<0>(GetParam());
   options.use_mixed_precision_solves = ::testing::get<1>(GetParam());
   const int kNumRefinementSteps = 4;
