a69baf90a3f96abc2b8d89a850446e6b3b1359cf
Dmitriy Korchemkin <dmitriy.korchemkin@gmail.com>
2022-08-18 22:10:17 +0300

    Change index order in parallel for
    Several modifications of ParallelFor implementation using CXX threads:
     - Index order changed from interleaved to sequential
     - Static task scheduling replaced with dynamic (controlled by
       kWorkItemsPerThread)
     - Changed index retrieval to atomic
    
    Changing index order from interleaved to sequential in parallel seem
    to significantly improve run-times of parallel loops, for example in
    evaluation of jacobian and residuals.
    
    Other modifications provide minor improvements for unbalanced
    sub-problem lengths and parallel for loops with small number of
    computation per operation.
    
    On BAL dataset the following improvements in time consumed for
    evaluation of residuals or jacobian and residuals were observed:
    
                                            OLD         NEW       OLD/NEW
                        dataset threads     r     J     r     J     r     J
    problem-257-65132-pre.txt         2 0.035 0.117 0.026 0.098 1.305 1.185
    problem-257-65132-pre.txt         4 0.022 0.082 0.017 0.065 1.285 1.268
    problem-257-65132-pre.txt         8 0.013 0.057 0.010 0.044 1.216 1.281
    problem-257-65132-pre.txt        16 0.008 0.049 0.007 0.040 1.105 1.204
    problem-356-226730-pre.txt        2 0.190 0.679 0.157 0.579 1.210 1.171
    problem-356-226730-pre.txt        4 0.121 0.465 0.084 0.353 1.439 1.314
    problem-356-226730-pre.txt        8 0.072 0.321 0.055 0.242 1.310 1.322
    problem-356-226730-pre.txt       16 0.045 0.249 0.036 0.212 1.256 1.176
    problem-1778-993923-pre.txt       2 0.827 2.800 0.635 2.300 1.300 1.217
    problem-1778-993923-pre.txt       4 0.525 1.911 0.401 1.451 1.309 1.317
    problem-1778-993923-pre.txt       8 0.296 1.317 0.234 1.064 1.262 1.237
    problem-1778-993923-pre.txt      16 0.197 1.200 0.161 1.010 1.219 1.188
    problem-1723-156502-pre.txt       2 0.102 0.374 0.084 0.313 1.201 1.192
    problem-1723-156502-pre.txt       4 0.068 0.243 0.054 0.199 1.246 1.219
    problem-1723-156502-pre.txt       8 0.040 0.174 0.030 0.139 1.298 1.250
    problem-1723-156502-pre.txt      16 0.025 0.140 0.020 0.121 1.225 1.156
    
    Run time in seconds for a single evaluation, using bundle_adjuster as:
    numactl -N 0 -m 0 ./bin/bundle_adjuster --num_threads ${t}  --input ${p}
    
    Note: performance when running across numa-nodes degrades in both old
    and proposed implementations, thus the test was executed limiting memory
    and compute resources allocation to a single numa-node.
    
    Change-Id: Ia195580bdab9d05c95ac983bfe37b045eecfaf49

--- internal/ceres/parallel_for_cxx.cc
+++ internal/ceres/parallel_for_cxx.cc
@@ -33,6 +33,7 @@
 
 #ifdef CERES_USE_CXX_THREADS
 
+#include <atomic>
 #include <cmath>
 #include <condition_variable>
 #include <memory>
@@ -52,58 +53,87 @@ namespace {
 // work.
 class BlockUntilFinished {
  public:
-  explicit BlockUntilFinished(int num_total)
-      : num_finished_(0), num_total_(num_total) {}
+  explicit BlockUntilFinished(int num_total_jobs)
+      : num_total_jobs_finished_(0), num_total_jobs_(num_total_jobs) {}
 
-  // Increment the number of jobs that have finished and signal the blocking
-  // thread if all jobs have finished.
-  void Finished() {
+  // Increment the number of jobs that have been processed by the number of
+  // jobs processed by caller and signal the blocking thread if all jobs
+  // have finished.
+  void Finished(int num_jobs_finished) {
+    if (!num_jobs_finished) return;
     std::lock_guard<std::mutex> lock(mutex_);
-    ++num_finished_;
-    CHECK_LE(num_finished_, num_total_);
-    if (num_finished_ == num_total_) {
+    num_total_jobs_finished_ += num_jobs_finished;
+    CHECK_LE(num_total_jobs_finished_, num_total_jobs_);
+    if (num_total_jobs_finished_ == num_total_jobs_) {
       condition_.notify_one();
     }
   }
 
-  // Block until all threads have signaled they are finished.
+  // Block until receiving confirmation of all jobs being finished.
   void Block() {
     std::unique_lock<std::mutex> lock(mutex_);
-    condition_.wait(lock, [&]() { return num_finished_ == num_total_; });
+    condition_.wait(
+        lock, [&]() { return num_total_jobs_finished_ == num_total_jobs_; });
   }
 
  private:
   std::mutex mutex_;
   std::condition_variable condition_;
   // The current number of jobs finished.
-  int num_finished_;
+  int num_total_jobs_finished_;
   // The total number of jobs.
-  int num_total_;
+  int num_total_jobs_;
 };
 
 // Shared state between the parallel tasks. Each thread will use this
 // information to get the next block of work to be performed.
-struct SharedState {
-  SharedState(int start, int end, int num_work_items)
+struct ThreadPoolState {
+  // The entire range [start, end) is split into num_work_blocks contiguous
+  // disjoint intervals (blocks), which are as equal as possible given
+  // total index count and requested number of  blocks.
+  //
+  // Those num_work_blocks blocks are then processed by num_workers
+  // workers
+  //
+  // Total number of integer indices in interval [start, end) is
+  // end - start, and when splitting them into num_work_blocks blocks
+  // we can either
+  //  - Split into equal blocks when (end - start) is divisable by
+  //    num_work_blocks
+  //  - Split into blocks with size difference at most 1:
+  //     - Size of the smallest block(s) is (end - start) / num_work_blocks
+  //     - (end - start) % num_work_blocks will need to be 1 index larger
+  //
+  // Note that this splitting is optimal in the sense of maximal difference
+  // between block sizes, since splitting into equal blocks is possible
+  // if and only if number of indices is divisable by number of blocks.
+  ThreadPoolState(int start, int end, int num_work_blocks, int num_workers)
       : start(start),
         end(end),
-        num_work_items(num_work_items),
+        num_work_blocks(num_work_blocks),
+        tail_block_size((end - start) / num_work_blocks),
+        num_full_sized_blocks((end - start) % num_work_blocks),
         i(0),
-        thread_token_provider(num_work_items),
-        block_until_finished(num_work_items) {}
+        thread_token_provider(num_workers),
+        block_until_finished(num_work_blocks) {}
 
   // The start and end index of the for loop.
   const int start;
   const int end;
   // The number of blocks that need to be processed.
-  const int num_work_items;
+  const int num_work_blocks;
+  // Size of the smallest block
+  const int tail_block_size;
+  // Number of blocks of size tail_block_size + 1
+  const int num_full_sized_blocks;
 
   // The next block of work to be assigned to a worker.  The parallel for loop
-  // range is split into num_work_items blocks of work, i.e. a single block of
-  // work is:
-  //  for (int j = start + i; j < end; j += num_work_items) { ... }.
-  int i;
-  std::mutex mutex_i;
+  // range is split into num_work_blocks blocks of work, with a single block of
+  // work being of size
+  //  - tail_block_size + 1 for the first num_full_sized_blocks blocks
+  //  - tail_block_size for the rest blocks
+  //  blocks of indices are contiguous and disjoint
+  std::atomic<int> i;
 
   // Provides a unique thread ID among all active threads working on the same
   // group of tasks.  Thread-safe.
@@ -117,6 +147,11 @@ struct SharedState {
 
 int MaxNumThreadsAvailable() { return ThreadPool::MaxNumThreadsAvailable(); }
 
+// Maximal number of work items scheduled for a single thread
+//  - Lower number of work items results in larger runtimes on unequal tasks
+//  - Higher number of work items results in larger losses for synchronization
+const int kWorkBlocksPerThread = 4;
+
 // See ParallelFor (below) for more details.
 void ParallelFor(ContextImpl* context,
                  int start,
@@ -124,10 +159,6 @@ void ParallelFor(ContextImpl* context,
                  int num_threads,
                  const std::function<void(int)>& function) {
   CHECK_GT(num_threads, 0);
-  CHECK(context != nullptr);
-  if (end <= start) {
-    return;
-  }
 
   // Fast path for when it is single threaded.
   if (num_threads == 1) {
@@ -145,9 +176,10 @@ void ParallelFor(ContextImpl* context,
 
 // This implementation uses a fixed size max worker pool with a shared task
 // queue. The problem of executing the function for the interval of [start, end)
-// is broken up into at most num_threads blocks and added to the thread pool. To
-// avoid deadlocks, the calling thread is allowed to steal work from the worker
-// pool. This is implemented via a shared state between the tasks. In order for
+// is broken up into at most num_threads * kWorkBlocksPerThread blocks
+// and added to the thread pool. To avoid deadlocks, the calling thread is
+// allowed to steal work from the worker pool.
+// This is implemented via a shared state between the tasks. In order for
 // the calling thread or thread pool to get a block of work, it will query the
 // shared state for the next block of work to be done. If there is nothing left,
 // it will return. We will exit the ParallelFor call when all of the work has
@@ -166,11 +198,6 @@ void ParallelFor(ContextImpl* context,
                  int num_threads,
                  const std::function<void(int thread_id, int i)>& function) {
   CHECK_GT(num_threads, 0);
-  CHECK(context != nullptr);
-  if (end <= start) {
-    return;
-  }
-
   // Fast path for when it is single threaded.
   if (num_threads == 1) {
     // Even though we only have one thread, use the thread token provider to
@@ -184,43 +211,77 @@ void ParallelFor(ContextImpl* context,
     return;
   }
 
+  CHECK(context != nullptr);
+  if (end <= start) {
+    return;
+  }
+
+  // Interval [start, end) is being split into
+  // num_threads * kWorkBlocksPerThread contiguous disjoint blocks.
+  //
+  // In order to avoid creating empty blocks of work, we need to limit
+  // number of work blocks by a total number of indices.
+  const int num_work_blocks =
+      std::min((end - start), num_threads * kWorkBlocksPerThread);
+
   // We use a std::shared_ptr because the main thread can finish all
   // the work before the tasks have been popped off the queue.  So the
   // shared state needs to exist for the duration of all the tasks.
-  const int num_work_items = std::min((end - start), num_threads);
-  std::shared_ptr<SharedState> shared_state(
-      new SharedState(start, end, num_work_items));
+  std::shared_ptr<ThreadPoolState> shared_state(
+      new ThreadPoolState(start, end, num_work_blocks, num_threads));
 
-  // A function which tries to perform a chunk of work. This returns false if
-  // there is no work to be done.
+  // A function which tries to perform several chunks of work.
   auto task_function = [shared_state, &function]() {
-    int i = 0;
-    {
-      // Get the next available chunk of work to be performed. If there is no
-      // work, return false.
-      std::lock_guard<std::mutex> lock(shared_state->mutex_i);
-      if (shared_state->i >= shared_state->num_work_items) {
-        return false;
-      }
-      i = shared_state->i;
-      ++shared_state->i;
-    }
-
+    int num_jobs_finished = 0;
     const ScopedThreadToken scoped_thread_token(
         &shared_state->thread_token_provider);
     const int thread_id = scoped_thread_token.token();
+    const int start = shared_state->start;
+    const int end = shared_state->end;
+    const int tail_block_size = shared_state->tail_block_size;
+    const int num_full_sized_blocks = shared_state->num_full_sized_blocks;
 
-    // Perform each task.
-    for (int j = shared_state->start + i; j < shared_state->end;
-         j += shared_state->num_work_items) {
-      function(thread_id, j);
+    while (true) {
+      // Get the next available chunk of work to be performed. If there is no
+      // work, return.
+      int i = shared_state->i++;
+      if (i >= shared_state->num_work_blocks) {
+        break;
+      }
+      ++num_jobs_finished;
+
+      // For-loop interval [start, end) was split into num_work_blocks,
+      // with num_full_sized_blocks of size tail_block_size + 1 and remaining
+      // num_work_blocks - num_full_sized_blocks of size tail_block_size
+      //
+      // Then, start index of the i-th block is given by a total
+      // length of preceeding blocks:
+      //  * Total length of preceeding blocks of size tail_block_size + 1:
+      //     min(i, num_full_sized_blocks) * (tail_block_size + 1)
+      //
+      //  * Total length of preceeding blocks of size tail_block_size:
+      //     (i - min(i, num_full_sized_blocks)) * tail_block_size
+      //
+      // Simplifying sum of those quantities yields a following
+      // expression for start index of the ith-block
+      const int curr_start =
+          start + i * tail_block_size + std::min(i, num_full_sized_blocks);
+      // First num_full_sized_blocks have size tail_block_size + 1
+      //
+      // Note that it is guaranteed that all blocks are within
+      // [start, end) interval
+      const int curr_end =
+          curr_start + tail_block_size + (i < num_full_sized_blocks ? 1 : 0);
+      // Perform each task in current block
+      for (int j = curr_start; j < curr_end; ++j) {
+        function(thread_id, j);
+      }
     }
-    shared_state->block_until_finished.Finished();
-    return true;
+    shared_state->block_until_finished.Finished(num_jobs_finished);
   };
 
   // Add all the tasks to the thread pool.
-  for (int i = 0; i < num_work_items; ++i) {
+  for (int i = 0; i < num_threads; ++i) {
     // Note we are taking the task_function as value so the shared_state
     // shared pointer is copied and the ref count is increased. This is to
     // prevent it from being deleted when the main thread finishes all the
@@ -231,8 +292,7 @@ void ParallelFor(ContextImpl* context,
   // Try to do any available work on the main thread. This may steal work from
   // the thread pool, but when there is no work left the thread pool tasks
   // will be no-ops.
-  while (task_function()) {
-  }
+  task_function();
 
   // Wait until all tasks have finished.
   shared_state->block_until_finished.Block();
