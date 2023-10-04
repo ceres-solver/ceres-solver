923de6dc5dbd271aea4d3839cee9e772a0e11035
Dmitriy Korchemkin <dmitriy.korchemkin@gmail.com>
2023-10-04 13:36:13 +0000

    Fix checks for CUDA memory pools support
    Change-Id: Icc07625fc0e586e8798da48fa5edfde59487d702

--- internal/ceres/context_impl.h
+++ internal/ceres/context_impl.h
@@ -134,6 +134,7 @@ class CERES_NO_EXPORT ContextImpl final : public Context {
   bool is_cuda_initialized_ = false;
   int gpu_device_id_in_use_ = -1;
   cudaDeviceProp gpu_device_properties_;
+  int cuda_memory_pools_supported_ = 0;
   int cuda_version_major_ = 0;
   int cuda_version_minor_ = 0;
 #endif  // CERES_NO_CUDA
