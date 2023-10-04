1f10442abb494e5cc6bdddd0a87714f9002829d2
Sergiu Deitsch <sergiu.deitsch@gmail.com>
2023-10-04 22:45:42 +0200

    Drop use of POSIX M_PI
    Change-Id: I37342a366161bb13d6456ecc67569fe12705e05c

--- examples/slam/pose_graph_2d/normalize_angle.h
+++ examples/slam/pose_graph_2d/normalize_angle.h
@@ -41,9 +41,9 @@ namespace ceres::examples {
 template <typename T>
 inline T NormalizeAngle(const T& angle_radians) {
   // Use ceres::floor because it is specialized for double and Jet types.
-  T two_pi(2.0 * M_PI);
+  T two_pi(2.0 * constants::pi);
   return angle_radians -
-         two_pi * ceres::floor((angle_radians + T(M_PI)) / two_pi);
+         two_pi * ceres::floor((angle_radians + T(constants::pi)) / two_pi);
 }
 
 }  // namespace ceres::examples
