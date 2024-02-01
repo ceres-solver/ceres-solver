62c03d6ff3b1735d4b0a88006486cfde39f10063
Sameer Agarwal <sameeragarwal@google.com>
2024-02-01 05:00:25 +0000

    Revert "Update Eigen::BDCSVD usage to comply with Eigen 3.4"
    This reverts commit 4027f6997521ac1e8b033a3534d0f8912533970b.
    
    Reason for revert: This breaks the build because this constructor syntax is only present in the unstable/development version of eigen. So the macro check is incorrect.
    
    Change-Id: I1e664208c7c50041e295aeb7c4b65259f9f2fc8f

--- internal/ceres/covariance_impl.cc
+++ internal/ceres/covariance_impl.cc
@@ -723,13 +723,8 @@ bool CovarianceImpl::ComputeCovarianceValuesUsingDenseSVD() {
   }
   event_logger.AddEvent("ConvertToDenseMatrix");
 
-#if EIGEN_VERSION_AT_LEAST(3, 4, 0)
-  Eigen::BDCSVD<Matrix, Eigen::ComputeThinU | Eigen::ComputeThinV> svd(
-      dense_jacobian);
-#else
   Eigen::BDCSVD<Matrix> svd(dense_jacobian,
                             Eigen::ComputeThinU | Eigen::ComputeThinV);
-#endif
 
   event_logger.AddEvent("SingularValueDecomposition");
 
