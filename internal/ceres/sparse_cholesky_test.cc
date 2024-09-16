648c09e91d91ba107d5c2db34fdaeaed8d40e312
Sergiu Deitsch <sergiu.deitsch@gmail.com>
2024-09-16 22:54:32 +0200

    Fix MSVC build
    Using preprocessor directives in a macro expansion is a (non-standard)
    language extension that is not supported by MSVC.
    
    Change-Id: I6e158b108a9c13cd277afb96cab2017ac192e3a2

--- internal/ceres/sparse_cholesky_test.cc
+++ internal/ceres/sparse_cholesky_test.cc
@@ -259,21 +259,21 @@ INSTANTIATE_TEST_SUITE_P(
 #endif
 
 #ifdef CERES_USE_EIGEN_SPARSE
-INSTANTIATE_TEST_SUITE_P(
-    EigenSparseCholesky,
-    SparseCholeskyTest,
-    ::testing::Combine(::testing::Values(EIGEN_SPARSE),
-                       ::testing::Values(false, true),
+const auto Orderings = ::testing::Combine(
+    ::testing::Values(EIGEN_SPARSE),
+    ::testing::Values(false, true),
 #if defined(CERES_NO_EIGEN_METIS)
-                       ::testing::Values(OrderingType::AMD,
-                                         OrderingType::NATURAL),
+    ::testing::Values(OrderingType::AMD, OrderingType::NATURAL),
 #else
-                       ::testing::Values(OrderingType::AMD,
-                                         OrderingType::NATURAL,
-                                         OrderingType::NESDIS),
+    ::testing::Values(
+        OrderingType::AMD, OrderingType::NATURAL, OrderingType::NESDIS),
 #endif  // defined(CERES_NO_EIGEN_METIS)
-                       ::testing::Values(true, false)),
-    ParamInfoToString);
+    ::testing::Values(true, false));
+
+INSTANTIATE_TEST_SUITE_P(EigenSparseCholesky,
+                         SparseCholeskyTest,
+                         Orderings,
+                         ParamInfoToString);
 #endif  // CERES_USE_EIGEN_SPARSE
 
 #ifndef CERES_NO_CUDSS
