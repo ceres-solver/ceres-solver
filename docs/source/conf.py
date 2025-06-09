0904f4ef85dc184ee39e3523c37bed2be9aa1cfb
Sergiu Deitsch <sergiu.deitsch@gmail.com>
2025-06-09 15:02:47 +0200

    Use subsections instead of an enumeration in solving FAQ
    This is a follow-up to related changes made in 018bb49e80ae3d1584fcb343fc31d513cbeb958c.
    
    Change-Id: Ife3befe85d7cea5dad5185a424508d590c0d77f7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Ceres Solver'
-copyright = u'2024 Google Inc'
+copyright = u'2025 Google Inc'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
